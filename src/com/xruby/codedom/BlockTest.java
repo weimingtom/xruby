b41c27b3066cf5ce03732fe646d476ff94697083
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-09-22 21:38:19 +0000

    test_do_end_is_same_as_curly_braces

--- src/com/xruby/codedom/BlockTest.java
+++ src/com/xruby/codedom/BlockTest.java
@@ -8,8 +8,8 @@ package com.xruby.codedom;
 public class BlockTest extends TestingAstTestCase {
 	public void test_parameter() {
 		Program p = getProgram("f {|a, b, c|   puts a  }");
-		CodePrinter CodePrinter = new CodePrinter();
-		p.accept(CodePrinter);
+		CodePrinter cp = new CodePrinter();
+		p.accept(cp);
 		String expected_result = 
 			"self\n" +
 			"block:3:false\n" +
@@ -25,13 +25,13 @@ public class BlockTest extends TestingAstTestCase {
 			"end block:true\n" +
 			"f:false\n" +
 			"EOF";
-		assertEquals(expected_result, CodePrinter.toString());
+		assertEquals(expected_result, cp.toString());
 	}
 	
 	public void test_dot_time() {
 		Program p = getProgram("3.times {print 'Ho!'}");
-		CodePrinter CodePrinter = new CodePrinter();
-		p.accept(CodePrinter);
+		CodePrinter cp = new CodePrinter();
+		p.accept(cp);
 		String expected_result = 
 			"3\n" +
 			"block:0:false\n" +
@@ -44,13 +44,13 @@ public class BlockTest extends TestingAstTestCase {
 			"end block:true\n" +
 			"times:true\n" +
 			"EOF";
-		assertEquals(expected_result, CodePrinter.toString());
+		assertEquals(expected_result, cp.toString());
 	}
 	
 	public void test_asterisk_parameter() {
 		Program p = getProgram("f {|a, b, *c|   puts c  }");
-		CodePrinter CodePrinter = new CodePrinter();
-		p.accept(CodePrinter);
+		CodePrinter codeprinter = new CodePrinter();
+		p.accept(codeprinter);
 		String expected_result = 
 			"self\n" +
 			"block:2:true\n" +
@@ -66,6 +66,18 @@ public class BlockTest extends TestingAstTestCase {
 			"end block:true\n" +
 			"f:false\n" +
 			"EOF";
-		assertEquals(expected_result, CodePrinter.toString());
+		assertEquals(expected_result, codeprinter.toString());
+	}
+	
+	public void test_do_end_is_same_as_curly_braces() {
+		Program p1 = getProgram("f {|a, b, *c|   puts c  }");
+		CodePrinter cp1 = new CodePrinter();
+		p1.accept(cp1);
+		
+		Program p2 = getProgram("f do |a, b, *c|   puts c  end");
+		CodePrinter cp2 = new CodePrinter();
+		p2.accept(cp2);
+		
+		assertEquals(cp1.toString(), cp2.toString());
 	}
 }
