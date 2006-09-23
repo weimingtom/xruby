cd70f2f99bb300032fa6004bef623b98a49ded74
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-09-23 01:34:41 +0000

    add BreakExpression

---
+++ trunk/src/com/xruby/codedom/BreakExpression.java
@@ -0,0 +1,23 @@
+package com.xruby.codedom;
+
+public class BreakExpression extends Expression {
+
+private ReturnArguments arguments_;
+	
+	public BreakExpression(ReturnArguments arguments){
+		arguments_ = arguments;
+	}
+	
+	public void accept(CodeVisitor visitor) {
+		visitor.visitBreakBegin();
+		
+		if (null != arguments_) { 
+			arguments_.accept(visitor);
+		} else {
+			visitor.visitNilExpression();
+		}
+
+		visitor.visitBreakEnd();
+	}
+
+}
