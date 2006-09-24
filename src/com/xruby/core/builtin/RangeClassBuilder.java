14f828559f69781d3c89ef0a502a313033e48bbc
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-09-24 20:12:13 +0000

    add Range#===

--- src/com/xruby/core/builtin/RangeClassBuilder.java
+++ src/com/xruby/core/builtin/RangeClassBuilder.java
@@ -3,21 +3,32 @@ package com.xruby.core.builtin;
 import com.xruby.core.lang.*;
 import com.xruby.core.value.*;
 
-/*class Range_case_equal extends RubyMethod {
+class Range_case_equal extends RubyMethod {
 	public Range_case_equal() {
 		super(1);
 	}
 
 	protected RubyValue run(RubyValue receiver, ArrayValue args, RubyBlock block) throws RubyException {
-		return ObjectFactory.falseValue;
+		Object o = args.get(0).getValue();
+		if (!(o instanceof IntegerValue)) {
+			//not comparable
+			return ObjectFactory.falseValue;
+		}
+
+		RangeValue r = (RangeValue)receiver.getValue();
+		if (r.include(((IntegerValue)o).intValue())) {
+			return ObjectFactory.trueValue;
+		} else {
+			return ObjectFactory.falseValue;
+		}
 	}
-}*/
+}
 
 public class RangeClassBuilder {
 	
 	public static RubyClass create() {
 		RubyClass c = RubyRuntime.GlobalScope.defineNewClass("Range", RubyRuntime.ObjectClass);
-		//c.defineMethod("===", new Range_case_equal());
+		c.defineMethod("===", new Range_case_equal());
 		return c;
 	}
 }
