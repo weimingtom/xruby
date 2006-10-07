afba0a20ad48c42f9f39b2fbfe5843ad05e6af71
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-10-07 20:41:00 +0000

    class name and module name should be stored in symbol table

--- trunk/src/com/xruby/core/builtin/ModuleClassBuilder.java
+++ trunk/src/com/xruby/core/builtin/ModuleClassBuilder.java
@@ -75,7 +75,7 @@ class Module_to_s extends RubyMethod {
 	}
 	
 	protected RubyValue run(RubyValue receiver, ArrayValue args, RubyBlock block) throws RubyException {
-		RubyClass c = (RubyClass)receiver.getValue();
+		MethodCollection c = (MethodCollection)receiver.getValue();
 		return ObjectFactory.createString(c.getName());
 	}
 }
