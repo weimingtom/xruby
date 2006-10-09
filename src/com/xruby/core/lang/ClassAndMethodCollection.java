8fa8613fdd92cc7460362ef2c8485c5cfb964a47
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-10-09 19:28:57 +0000

    should throw TypeErrorClass when super class mismatch more comments

--- src/com/xruby/core/lang/ClassAndMethodCollection.java
+++ src/com/xruby/core/lang/ClassAndMethodCollection.java
@@ -10,25 +10,32 @@ import java.util.Map;
 abstract class ClassAndMethodCollection extends MethodCollection {
 	protected Map<String, RubyClass> classes_ = new HashMap<String, RubyClass>();
 
+	/// This method is only used by java classes in package 'com.xruby.core.builtin'.
+	/// It has less overhead than 'defineClass' (no hash table lookup).
+	/// This method is NOT used by classes compiled from ruby script.
 	public RubyClass defineNewClass(String name, RubyClass parent) {
 		RubyClass c = new RubyClass(name, parent);
 		classes_.put(name, c);
 		return c;
 	}
 
-	/// define a new class or get a old one
 	private RubyClass defineClass(String name, RubyClass parent) throws RubyException {
 		RubyClass c = classes_.get(name);
 		if (null == c) {
 			c = new RubyClass(name, parent);
 			classes_.put(name, c);
-		} else if (!c.isMyParent(parent)){
-			throw new RubyException(RubyRuntime.NameErrorClass, "superclass mismatch for class "+ name + " (TypeError)");
+			return c;
 		}
-		
+
+		if (!c.isMyParent(parent)){
+			throw new RubyException(RubyRuntime.TypeErrorClass, "superclass mismatch for class "+ name);
+		}
+
+		c.setAccessPublic();
 		return c;
 	}
 
+	/// define a new class or get a old one
 	public RubyClass defineClass(String name, RubyValue parent) throws RubyException {
 		if (parent.getRubyClass() != RubyRuntime.ClassClass) {
 			throw new RubyException(RubyRuntime.TypeErrorClass, "superclass must be a Class (" + parent.getRubyClass().getName() + " given)");
