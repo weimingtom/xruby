b1cf358a2d693fa44b35440540b0b70dd1e0ff43
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-10-09 19:41:38 +0000

    add and use defineNewModule

--- src/com/xruby/core/lang/ModuleClassAndMethodCollection.java
+++ src/com/xruby/core/lang/ModuleClassAndMethodCollection.java
@@ -10,17 +10,25 @@ import java.util.Map;
 class ModuleClassAndMethodCollection extends ClassAndMethodCollection {
 	protected Map<String, RubyModule> modules_ = new HashMap<String, RubyModule>();
 
+	public RubyModule defineNewModule(String name) {
+		RubyModule m = new RubyModule(name);
+		modules_.put(name, m);
+		return m;
+	}
+
 	public RubyModule defineModule(String name) {
 		RubyModule m = modules_.get(name);
 		if (null == m) {
-			m = new RubyModule(name);
-			modules_.put(name, m);
+			return defineNewModule(name);
 		}
 
 		return m;
 	}
 
-	public RubyModule getModule(String name) {
-		return modules_.get(name);
+	/// e.g. A::B
+	public RubyValue getConstantViaColon(String name) throws RubyException {
+		//TODO
+		throw new RubyException(RubyRuntime.NameErrorClass, "uninitialized constant " + name_ + "::" + name);
 	}
 }
+
