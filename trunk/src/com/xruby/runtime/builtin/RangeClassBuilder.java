75dca77cfeb23406d37f251aaf85306ab84f2982
dreamhead.cn <dreamhead.cn@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-10-17 15:21:32 +0000

    add Array#pop and Range#to_s

--- trunk/src/com/xruby/runtime/builtin/RangeClassBuilder.java
+++ trunk/src/com/xruby/runtime/builtin/RangeClassBuilder.java
@@ -24,11 +24,26 @@ class Range_case_equal extends RubyMethod {
 	}
 }
 
+class Range_to_s extends RubyMethod {
+	public Range_to_s() {
+		super(-1);
+	}
+
+	protected RubyValue run(RubyValue receiver, ArrayValue args, RubyBlock block) throws RubyException {
+		RangeValue r = (RangeValue)receiver.getValue();
+		int left = r.getLeft();
+		int right = r.getRight();
+		
+		return ObjectFactory.createString(left + "..." + right);
+	}
+}
+
 public class RangeClassBuilder {
 	
 	public static RubyClass create() {
 		RubyClass c = RubyRuntime.GlobalScope.defineNewClass("Range", RubyRuntime.ObjectClass);
 		c.defineMethod("===", new Range_case_equal());
+		c.defineMethod("to_s", new Range_to_s());
 		return c;
 	}
 }
