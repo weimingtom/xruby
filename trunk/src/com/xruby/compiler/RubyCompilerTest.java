19b96dd4f04a3f6c442c43299c6e4da2b6a62eec
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-10-18 04:39:24 +0000

    add Kernel#send, __send__

--- trunk/src/com/xruby/compiler/RubyCompilerTest.java
+++ trunk/src/com/xruby/compiler/RubyCompilerTest.java
@@ -2348,5 +2348,18 @@ public class RubyCompilerTest extends TestCase {
 		
 		compile_run_and_compare_output(program_texts, outputs);
 	}
-	
+
+	public void test_send() {
+		String [] program_texts = {
+				"print 'xx'.send(:length)",
+				"print 1.__send__(:+, 2)",
+		};
+
+		String[] outputs = {
+				"2",
+				"3",
+		};
+		
+		compile_run_and_compare_output(program_texts, outputs);
+	}
 }
