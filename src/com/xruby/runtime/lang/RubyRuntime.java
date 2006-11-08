87ec0cab6db8f4b2153ff437c4d452b8724cfe77
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-11-08 01:56:43 +0000

    move static methods from ArrayValue to RubyRuntime

--- src/com/xruby/runtime/lang/RubyRuntime.java
+++ src/com/xruby/runtime/lang/RubyRuntime.java
@@ -183,5 +183,41 @@ public class RubyRuntime {
 			throw new RubyException(RubyRuntime.ExceptionClass, e.toString());
 		}
 	}
+	
+	public static RubyValue expandArrayIfThereIsZeroOrOneValue(ArrayValue a) {
+		if (a.size() <= 1) {
+			return a.get(0);
+		} else {
+			return ObjectFactory.createArray(a);
+		}
+	}
+
+	public static RubyValue expandArrayIfThereIsZeroOrOneValue(RubyValue v) {
+		if (v.getValue() instanceof ArrayValue) {
+			ArrayValue a = (ArrayValue)v.getValue();
+			if (!a.notSingleAsterisk()) {
+				return expandArrayIfThereIsZeroOrOneValue(a);
+			}
+		}
+
+		return v;
+	}
+	
+	public static ArrayValue expandArrayIfThereIsOnlyOneArrayValue(ArrayValue a) {
+		if (a.size() == 1 &&
+				a.get(0).getValue() instanceof ArrayValue) {
+			return (ArrayValue)a.get(0).getValue();
+		} else {
+			return a;
+		}
+	}
+
+	public static ArrayValue convertToArrayIfNotYet(RubyValue v) {
+		if (v.getValue() instanceof ArrayValue) {
+			return (ArrayValue)v.getValue();
+		} else {
+			return new ArrayValue(v);
+		}
+	}
 
 }
