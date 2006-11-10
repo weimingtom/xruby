a1bc8ab3aa9787dfdd01f40ad22d32cfb6c7bd6d
cpunion <cpunion@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-11-10 00:46:13 +0000

    implement Kernel#require to load ruby library. loading order is [$:, jar files]

--- src/com/xruby/runtime/builtin/FileClassBuilder.java
+++ src/com/xruby/runtime/builtin/FileClassBuilder.java
@@ -1,6 +1,26 @@
 package com.xruby.runtime.builtin;
 
+import java.io.File;
+
 import com.xruby.runtime.lang.*;
+import com.xruby.runtime.value.ArrayValue;
+import com.xruby.runtime.value.ObjectFactory;
+import com.xruby.runtime.value.StringValue;
+
+class File_is_file extends RubyMethod {
+	public File_is_file() {
+		super(1);
+	}
+
+	protected RubyValue run(RubyValue receiver, ArrayValue args, RubyBlock block) throws RubyException {
+		StringValue fileName = (StringValue)args.get(0).getValue();
+		File file = new File(fileName.toString());
+		if (file.isFile()){
+			return ObjectFactory.trueValue;
+		}
+		return ObjectFactory.falseValue;
+	}
+}
 
 public class FileClassBuilder {
 
@@ -8,5 +28,9 @@ public class FileClassBuilder {
 		RubyClass c = RubyRuntime.GlobalScope.defineNewClass("File", RubyRuntime.IOClass);
 		//c.defineMethod("id2name", new Symbol_id2name());
 		return c;
+	}
+	
+	public static void initSingletonMethods() {
+		ObjectFactory.FileClassValue.defineMethod("file?", new File_is_file());
 	}
 }
