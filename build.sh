88a14892dba0da3bfe635e922058984709b83a2b
beanworms <beanworms@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-11-13 08:43:38 +0000

    roll it back

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #For Cygwin, please use build.bat
 
-java -cp lib/ant/ant-1.6.5.jar:lib/ant/ant-launcher-1.6.5.jar:lib/ant/ant-antlr-1.6.5.jar:lib/ant/ant-junit-1.6.5.jar:lib/junit/junit-3.8.1.jar:"/usr/lib/jvm/jdk/lib/tools.jar" org.apache.tools.ant.Main $1
+java -cp lib/ant/ant-1.6.5.jar:lib/ant/ant-launcher-1.6.5.jar:lib/ant/ant-antlr-1.6.5.jar:lib/ant/ant-junit-1.6.5.jar:lib/junit/junit-3.8.1.jar:"$JAVA_HOME/lib/tools.jar" org.apache.tools.ant.Main $1
