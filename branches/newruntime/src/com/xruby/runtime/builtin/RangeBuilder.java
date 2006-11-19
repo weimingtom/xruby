b25cccc2abbf927c8e6e3675362b971515ad2c9a
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-11-19 20:51:35 +0000

    remove *Method and merge them into *Builder (we may split them in the future, but for now we need a consistent layout style among all builders)

--- branches/newruntime/src/com/xruby/runtime/builtin/RangeBuilder.java
+++ branches/newruntime/src/com/xruby/runtime/builtin/RangeBuilder.java
@@ -1,6 +1,10 @@
 package com.xruby.runtime.builtin;
 
 import com.xruby.runtime.lang.*;
+import com.xruby.runtime.value.ObjectFactory;
+import com.xruby.runtime.value.RubyArray;
+import com.xruby.runtime.value.RubyFixnum;
+import com.xruby.runtime.value.RubyRange;
 
 public class RangeBuilder implements ExtensionBuilder {
 	private RubyClass rangeClass;
@@ -35,4 +39,76 @@ public class RangeBuilder implements ExtensionBuilder {
 		this.rangeClass.defineMethod("include?", RangeMethod.include, 1);
 	}
 }
+
+class RangeMethod {
+	public static RubyMethod alloc = new RubyNoArgMethod() {
+		protected RubyValue run(RubyValue receiver) {
+			return new RubyRange();
+		}		
+	};
+	
+	public static RubyMethod initialize = new NoBlockRubyMethod() {
+		public RubyValue run(RubyValue receiver, RubyArray args) {
+			RubyRange range = (RubyRange)receiver;
+			int argc = (args == null) ? 0 : args.length();
+			
+			if (argc >= 2) {
+				int begin = ((RubyFixnum)args.get(0)).intValue();
+				int end = ((RubyFixnum)args.get(1)).intValue();
+				range.setBegin(begin);
+				range.setEnd(end);
+			}
+			
+			if (argc >= 3) {
+				range.setExcludeEnd(RubyAPI.test(args.get(2)));
+			}
+			
+			return RubyConstant.QNIL;			
+		}		
+	};
+	
+	public static RubyMethod include = new RubyOneArgMethod() {
+		protected RubyValue run(RubyValue receiver, RubyValue arg) {
+			RubyRange range = (RubyRange)receiver;
+			int value = ((RubyFixnum)arg).intValue();
+			return RubyAPI.test(range.include(value));
+		}		
+	};
+	
+	public static RubyMethod first = new RubyNoArgMethod() {
+		protected RubyValue run(RubyValue receiver) {
+			RubyRange range = (RubyRange)receiver;
+			return RubyFixnum.int2Fix(range.getBegin());
+		}
+	};
+	
+	public static RubyMethod last = new RubyNoArgMethod() {
+		protected RubyValue run(RubyValue receiver) {
+			RubyRange range = (RubyRange)receiver;
+			return RubyFixnum.int2Fix(range.getEnd());
+		}
+	};
+	
+	public static RubyMethod toS = new RubyNoArgMethod() {
+		protected RubyValue run(RubyValue receiver) {
+			RubyRange range = (RubyRange)receiver;
+			return ObjectFactory.createString(range.toString());
+		}
+	};
+	
+	public static RubyMethod excludeEnd = new RubyNoArgMethod() {
+		protected RubyValue run(RubyValue receiver) {
+			RubyRange range = (RubyRange)receiver;
+			return RubyAPI.test(range.isExcludeEnd());
+		}
+	};
+	
+	public static RubyMethod eq = new RubyOneArgMethod() {
+		protected RubyValue run(RubyValue receiver, RubyValue arg) {
+			RubyRange range = (RubyRange)receiver;
+			return RubyAPI.test(range.equals(arg));
+		}
+	};
+}
+
 
