129ed3a3e971129bff895e8019f609bf3d7ac08f
dreamhead.cn <dreamhead.cn@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-11-24 00:01:41 +0000

    refacotr class hierarchy

--- src/com/xruby/runtime/lang/RubyClass.java
+++ src/com/xruby/runtime/lang/RubyClass.java
@@ -2,7 +2,9 @@ package com.xruby.runtime.lang;
 
 import com.xruby.runtime.value.*;
 
-public class RubyClass extends RubyClassModuleBase {
+public abstract class RubyClass extends RubyClassModuleBase {	
+	public abstract boolean isSingleton();
+	
 	public RubyValue newInstance() {
 		return this.newInstance(RubyMethod.NULL_ARG);
 	}
@@ -11,15 +13,110 @@ public class RubyClass extends RubyClassModuleBase {
 		return this.callMethod("new", args);
 	}
 	
-	public void attachSingletonClass(RubyValue obj) {
-		if (this.isSingleton()) {
-			ensureIvTableNotNull();
-			RubyID id = StringMap.intern("__attached__");
-			this.ivTable.put(id, obj);			
-		}
-	}
-	
 	public void setName(RubyID id) {
 		this.setInstanceVariable("__classid__", id.toSymbol());
+	}
+	
+	// method invocation	
+	public RubyValue callMethod(RubyValue receiver, RubyID mid, RubyArray args, RubyBlock block) {
+		return invokeMethod(receiver, mid, args, RubyMethodScope.ALL);
+	}
+	
+	public RubyValue callPublicMethod(RubyValue receiver, RubyID mid, RubyArray args, RubyBlock block) {
+		return invokeMethod(receiver, mid, args, RubyMethodScope.ALL);
+	}
+	
+	public RubyValue callSuperMethod(RubyValue receiver, RubyID mid, RubyArray args, RubyBlock block) {
+		RubyClass base = this.getSuper();
+		return base.invokeMethod(receiver, mid, args, RubyMethodScope.SUPER);
+	}
+
+	private RubyValue invokeMethod(RubyValue receiver, RubyID mid, RubyArray args, RubyMethodScope scope) {
+		RubyMethodWrapper wrapper = this.findMethod(mid);
+		if (wrapper == null) {
+			return this.methodMissing(receiver, mid);
+		}
+		
+		switch (scope) {
+		case ALL:
+			break;
+		case PUBLIC:
+			if (wrapper.getAttr() != RubyMethodAttr.PUBLIC) {
+				return this.methodMissing(receiver, mid);
+			}
+		case PROTECTED:
+			break;
+		case PRIVATE:
+			break;
+		case SUPER:
+			break;
+		}
+		
+		int argc = wrapper.getArgc();
+		if (argc >= 0 && args != null && args.length() != argc) {
+			RubyAPI.raise(RubyRuntime.argumentError, "wrong number of arguments (%d for %d)", args.length(), argc);
+		}
+		
+		RubyMethod method = wrapper.getMethod();
+		if (method == null) {
+			return this.methodMissing(receiver, mid);
+		}
+		
+		return method.invoke(receiver, args);
+	}	
+	
+	private RubyMethodWrapper findMethod(RubyID mid) {
+		// find method in cache
+		MethodCache.CacheEntry entry = cache.getMethod(this, mid);
+		if (entry != null && entry.klass == this && entry.mid == mid) {
+			if (entry.method == null) {
+				return null;
+			} else {
+				return entry.method;
+			}
+		}
+		
+		RubyMethodWrapper wrapper = searchMethod(mid);
+		
+		// put method in cache
+		if (entry == null) {
+			entry = new MethodCache.CacheEntry();
+		}
+		
+		entry.klass = this;
+		entry.mid = mid;
+		entry.method = wrapper;
+	
+		return wrapper;		
+	}
+
+	private RubyMethodWrapper searchMethod(RubyID mid) {
+		// find method in method table
+		RubyClassModuleBase klass = this;
+		
+		while (klass != null) {	
+			RubyMethodWrapper method = klass.methodTable.get(mid);
+			
+			if (method != null) {
+				return method;
+			}
+			
+			klass = klass.superclass;
+		} 
+		
+		return null;
+	}
+	
+	private RubyValue methodMissing(RubyValue obj, RubyID id) {
+		if (id == RubyID.ID_ALLOCATOR) {
+			RubyAPI.raise(RubyRuntime.typeError, "allocator undefined for %s", obj.getRubyClassName().getString());
+		}
+		
+		// FIXME: invoke method missing
+		
+		RubyAPI.raise(RubyRuntime.noMethodError, "undefined method `%s' for %s", StringMap.id2name(id), obj.getRubyClassName());
+		
+		// unable reach here
+		return null;
 	}
 }
