909b7c4066d61c0ca0a5ef3a96f6f715264a9f27
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-12-19 02:02:48 +0000

    support ?\n, ?\r

--- trunk/src/com/xruby/compiler/codedom/AsciiValueExpression.java
+++ trunk/src/com/xruby/compiler/codedom/AsciiValueExpression.java
@@ -4,7 +4,24 @@ public class AsciiValueExpression extends Expression {
 	private int value_;
 	
 	public AsciiValueExpression(String value) {
-		value_ = (int)value.charAt(1);
+		if (value.charAt(1) == '\\') {
+			switch (value.charAt(2)) {
+				case 'n':
+					value_ = '\n';
+					return;
+				case 'r':
+					value_ = '\r';
+					return;
+				default:
+					//TODO more escped value
+					value_ = (int)value.charAt(2);
+					return;
+				
+			}
+		} else {
+			value_ = (int)value.charAt(1);
+			return;
+		}
 	}
 
 	public void accept(CodeVisitor visitor) {
