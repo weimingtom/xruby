caa1bb9e6cb7c77cbce463f47f082882f5a2409f
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2006-12-21 02:17:20 +0000

    implement Hash#has_key?, Hash#hash_value?

--- src/com/xruby/runtime/value/RubyHash.java
+++ src/com/xruby/runtime/value/RubyHash.java
@@ -16,38 +16,38 @@ import java.util.ArrayList;
  */
 public class RubyHash extends RubyBasic {
 	private Map<RubyValue, RubyValue> map_ = new HashMap<RubyValue, RubyValue>();
-    // To ensure the order
-    private List<RubyValue> keys_ = new ArrayList<RubyValue>();
+	// To ensure the order
+	private List<RubyValue> keys_ = new ArrayList<RubyValue>();
 
 	private RubyValue default_value_ = ObjectFactory.nilValue;
-    private RubyBlock block = null;
+	private RubyBlock block = null;
 
-    RubyHash() {
-    	super(RubyRuntime.HashClass);
-    }
-    
-    public void add(RubyValue k, RubyValue v) {
+	RubyHash() {
+		super(RubyRuntime.HashClass);
+	}
+
+	public void add(RubyValue k, RubyValue v) {
 		map_.put(k, v);
-        keys_.add(k);
-    }
+	    keys_.add(k);
+	}
 
 	public int size() {
 		return map_.size();
 	}
 
-    public RubyValue to_s() {
+	public RubyValue to_s() {
 		RubyString r = ObjectFactory.createString();
 
 		for (RubyValue key : map_.keySet()) {
-            RubyValue value = map_.get(key);
-            r.appendString(key.toString()+ value.toString());
+	        RubyValue value = map_.get(key);
+	        r.appendString(key.toString()+ value.toString());
 		}
 
 		return r;
 	}
 
 
-    public RubyValue get(RubyValue k) {
+	public RubyValue get(RubyValue k) {
 		RubyValue v = map_.get(k);
 		if (null == v) {
 			return default_value_;
@@ -56,45 +56,65 @@ public class RubyHash extends RubyBasic {
 		}
 	}
 
-    public void rb_iterate(RubyValue receiver, RubyBlock block) {
-        for(RubyValue key: keys_) {
-            RubyValue value = map_.get(key);
-            RubyArray args = new RubyArray();
-            args.add(key);
-            args.add(value);
+	public void rb_iterate(RubyValue receiver, RubyBlock block) {
+		for(RubyValue key: keys_) {
+			RubyValue value = map_.get(key);
+			RubyArray args = new RubyArray();
+			args.add(key);
+			args.add(value);
 
-            block.invoke(receiver, args);
-        }
-    }
+			block.invoke(receiver, args);
+		}
+	}
 
-    public RubyArray to_a() {
-        RubyArray array = new RubyArray();
-        for(RubyValue key: keys_) {
-            RubyValue value = map_.get(key);
-            RubyArray entry = new RubyArray();
-            entry.add(key);
-            entry.add(value);
-            array.add(entry);
-        }
+	public RubyArray to_a() {
+		RubyArray array = new RubyArray();
+		for(RubyValue key: keys_) {
+			RubyValue value = map_.get(key);
+			RubyArray entry = new RubyArray();
+			entry.add(key);
+			entry.add(value);
+			array.add(entry);
+		}
 
-        return array;
-    }
+		return array;
+	}
 
-    // Getter and Setter for default value
-    public RubyValue getDefaultValue() {
-        return default_value_;
-    }
+	public boolean has_key(RubyValue key) {
+		for (RubyValue v : keys_) {
+			if (RubyAPI.testEqual(key, v)) {
+				return true;
+			}
+		}
 
-    public void setDefaultValue(RubyValue defaultValue) {
-        this.default_value_ = defaultValue;
-    }
+		return false;
+	}
 
+	public boolean has_value(RubyValue value) {
+		for(RubyValue key: keys_) {
+			RubyValue v = map_.get(key);
+			if (RubyAPI.testEqual(value, v)) {
+				return true;
+			}
+		}
 
-    public RubyBlock getBlock() {
-        return block;
-    }
+		return false;
+	}
 
-    public void setBlock(RubyBlock block) {
-        this.block = block;
-    }
+	// Getter and Setter for default value
+	public RubyValue getDefaultValue() {
+		return default_value_;
+	}
+
+	public void setDefaultValue(RubyValue defaultValue) {
+		this.default_value_ = defaultValue;
+	}
+
+	public RubyBlock getBlock() {
+		return block;
+	}
+
+	public void setBlock(RubyBlock block) {
+		this.block = block;
+	}
 }
