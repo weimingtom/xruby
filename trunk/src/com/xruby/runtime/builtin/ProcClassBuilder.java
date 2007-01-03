b7fc4b6b51db99e0f287b3b50c43247e16683859
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-01-03 06:00:11 +0000

    add Proc#new

--- trunk/src/com/xruby/runtime/builtin/ProcClassBuilder.java
+++ trunk/src/com/xruby/runtime/builtin/ProcClassBuilder.java
@@ -14,10 +14,21 @@ class Proc_call extends RubyMethod {
 	}
 }
 
+class Proc_alloc extends RubyMethod {
+	public Proc_alloc() {
+		super(-1);
+	}
+	
+	protected RubyValue run(RubyValue receiver, RubyArray args, RubyBlock block) {
+		return ObjectFactory.createProc(block);
+	}
+}
+
 public class ProcClassBuilder {
 	
 	public static void initialize() {
 		RubyClass c = RubyRuntime.ProcClass;
 		c.defineMethod("call", new Proc_call());
+		c.defineAllocMethod(new Proc_alloc());
 	}
 }
