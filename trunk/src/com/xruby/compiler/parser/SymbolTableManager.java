f9165e46dfe8ef4496f7d2b89ea452e685291505
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-01-15 09:06:04 +0000

    rewrite the symbol table used by the parser as the old one has too many problems

---
+++ trunk/src/com/xruby/compiler/parser/SymbolTableManager.java
@@ -0,0 +1,38 @@
+package com.xruby.compiler.parser;
+
+import java.util.*;
+
+public class SymbolTableManager {
+	private Stack<SymbolTable> stack_ = new Stack<SymbolTable>();
+	
+	public SymbolTableManager(String[] pre_defined) {
+		enterScope();
+		
+		if (null == pre_defined) {
+			return;
+		}
+		for (String s : pre_defined) {
+			addVariable(s);
+		}
+	}
+	
+	public void enterScope() {
+		stack_.add(new SymbolTable());
+	}
+	
+	public void enterBlockScope() {
+		stack_.add(new SymbolTableForBlock(stack_.peek()));
+	}
+	
+	public void leaveScope() {
+		stack_.pop();
+	}
+	
+	public boolean isDefinedInCurrentScope(String s) {
+		return stack_.peek().findVariable(s);
+	}
+	
+	public void addVariable(String s) {
+		stack_.peek().addVariable(s);
+	}
+}
