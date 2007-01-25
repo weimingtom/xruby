020eb89e19c752bf5a07e9dc99a770cfa273b426
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-01-25 14:32:42 +0000

    use apache.oro to implement Dir#[]

--- src/com/xruby/runtime/builtin/DirClassBuilder.java
+++ src/com/xruby/runtime/builtin/DirClassBuilder.java
@@ -1,15 +1,9 @@
 package com.xruby.runtime.builtin;
 
 import java.io.File;
-
-import com.xruby.runtime.lang.RubyBlock;
-import com.xruby.runtime.lang.RubyClass;
-import com.xruby.runtime.lang.RubyException;
-import com.xruby.runtime.lang.RubyMethod;
-import com.xruby.runtime.lang.RubyRuntime;
-import com.xruby.runtime.lang.RubyValue;
-import com.xruby.runtime.value.ObjectFactory;
-import com.xruby.runtime.value.RubyArray;
+import org.apache.oro.io.GlobFilenameFilter;
+import com.xruby.runtime.lang.*;
+import com.xruby.runtime.value.*;
 
 class Dir_chdir extends RubyMethod {
 	public Dir_chdir() {
@@ -81,14 +75,32 @@ class Dir_entries extends RubyMethod {
 		if (!file.isDirectory()){
 			throw new RubyException(RubyRuntime.RuntimeErrorClass, "Not a directory - " + dir);
 		}
+
 		RubyArray files = new RubyArray();
 		for (String f: file.list()){
 			files.add(ObjectFactory.createString(f));
 		}
+		
 		return files;
 	}
 }
 
+class Dir_array_access extends RubyMethod {
+	public Dir_array_access() {
+		super(1);
+	}
+
+	protected RubyValue run(RubyValue receiver, RubyArray args, RubyBlock block) {
+		File file = new File(".");
+		String pattern = RubyTypesUtil.convertToString(args.get(0)).toString();
+		RubyArray files = new RubyArray();
+		for (String f: file.list(new GlobFilenameFilter(pattern))){
+			files.add(ObjectFactory.createString(f));
+		}
+		
+		return files;
+	}
+}
 
 public class DirClassBuilder {
 	public static void initialize() {
@@ -104,6 +116,7 @@ public class DirClassBuilder {
 		c.getSingletonClass().defineMethod("delete", rmdir);
 		c.getSingletonClass().defineMethod("unlink", rmdir);
 		c.getSingletonClass().defineMethod("entries", new Dir_entries());
+		c.getSingletonClass().defineMethod("[]", new Dir_array_access());
 	
 	}
 	
