beca00861d8be67508be939c2d2fbf007d5c19ad
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-01-27 01:50:47 +0000

    implement String#unpack('q')

--- src/com/xruby/runtime/builtin/ArrayPackerTest.java
+++ src/com/xruby/runtime/builtin/ArrayPackerTest.java
@@ -9,4 +9,17 @@ public class ArrayPackerTest extends TestCase {
 		String expected = new String(new char[] {1, 0, 0, 0, 0, 0, 0, 0});
 		assertEquals(expected.length(), s.toString().length());
 	}
+	
+	public void test_unpack_empty() {
+		RubyArray a = ArrayPacker.unpack("", "q");
+		assertEquals(1, a.size());
+		assertEquals(ObjectFactory.nilValue, a.get(0));
+	}
+	
+	public void test_unpack() {
+		String s = new String(new char[] {1, 0, 0, 0, 0, 0, 0, 0});
+		RubyArray a = ArrayPacker.unpack(s, "q");
+		assertEquals(1, a.size());
+		assertEquals(ObjectFactory.fixnum1, a.get(0));
+	}
 }
