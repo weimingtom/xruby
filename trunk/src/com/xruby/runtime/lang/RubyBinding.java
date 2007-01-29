145424d157f3263954f4988aa23810ffd1c24737
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-01-29 05:44:14 +0000

    update copyright block

--- trunk/src/com/xruby/runtime/lang/RubyBinding.java
+++ trunk/src/com/xruby/runtime/lang/RubyBinding.java
@@ -1,3 +1,8 @@
+/** 
+ * Copyright 2005-2007 Xue Yong Zhi
+ * Distributed under the GNU General Public License 2.0
+ */
+
 package com.xruby.runtime.lang;
 
 import com.xruby.runtime.value.*;
--- trunk/src/com/xruby/runtime/lang/RubyRuntime.java
+++ trunk/src/com/xruby/runtime/lang/RubyRuntime.java
@@ -1,5 +1,6 @@
-/**
- * Copyright (c) 2005-2006 Xue Yong Zhi. All rights reserved.
+/** 
+ * Copyright 2005-2007 Xue Yong Zhi
+ * Distributed under the GNU General Public License 2.0
  */
 
 package com.xruby.runtime.lang;
