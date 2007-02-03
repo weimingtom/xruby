4fd6791dbf643d43c00d4c92a3ab1fbe98100bd6
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-02-03 02:05:05 +0000

    rename TestUnitTest.java to StdlibTest.java

--- trunk/src/com/xruby/compiler/StdlibTest.java
+++ trunk/src/com/xruby/compiler/StdlibTest.java
@@ -2,13 +2,16 @@ package com.xruby.compiler;
 
 import com.xruby.runtime.lang.RubyRuntime;
 
-public class TestUnitTest extends CompilerTestCase {
+/**
+ * Test if ruby's standand lib work under xruby
+ */
+public class StdlibTest extends CompilerTestCase {
 	
 	public void setUp() {
 		RubyRuntime.initBuiltin(null);
 	}
 	
-	public void test_StandardError() {
+	public void test_test_unit_StandardError() {
 		String[] program_texts = {
 				"require 'test/unit/assertionfailederror'",
 				"print Test::Unit::AssertionFailedError.superclass",
@@ -22,15 +25,19 @@ public class TestUnitTest extends CompilerTestCase {
 		compile_run_and_compare_output(program_texts, outputs);
 	}
 	
-	public void test_Failure() {
+	public void test_test_unit_Failure() {
 		String[] program_texts = {
 				"require 'test/unit/failure'",
 				"print Test::Unit::Failure.new('x', 'y', 'z').single_character_display",
+				"print Test::Unit::Failure.new('x', 'y', 'z').short_display",
+				"print Test::Unit::Failure.new('x1', [1, 2], 'z1').long_display",
 		};
 		
 		String[] outputs = {
 				"",
 				"F",
+				"x: z",
+				"Failure:\nx1\n    [1\n     2]:\nz1",
 		};
 		
 		compile_run_and_compare_output(program_texts, outputs);
