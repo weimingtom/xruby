3e85f9504a18814a8a023721c7ce915e59bcac90
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-02-10 04:23:47 +0000

    use more RubyAPI

--- src/com/xruby/runtime/value/RubyRange.java
+++ src/com/xruby/runtime/value/RubyRange.java
@@ -18,14 +18,10 @@ public class RubyRange extends RubyBasic {
 	}
 
 	public void setValue(RubyValue left, RubyValue right, boolean isExclusive) {
-		RubyMethod m = left.findMethod("<=>");
-		if (m == null){
-			throw new RubyException(RubyRuntime.ArgumentErrorClass, "bad value for range");
-		}
 		RubyArray args = new RubyArray();
 		args.add(right);
 		try {
-			RubyValue result = m.invoke(left, args, null);
+			RubyValue result = RubyAPI.callMethod(left, args, null, "<=>");
 			if (result == ObjectFactory.nilValue){
 				throw new RubyException(RubyRuntime.ArgumentErrorClass, "bad value for range");
 			}
