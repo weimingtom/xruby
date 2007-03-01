5976bdf66d4a9cc655f46a81ebdb797fc2b62d58
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-03-01 00:26:41 +0000

    0.1.2

--- benchmark/run.rb
+++ benchmark/run.rb
@@ -37,7 +37,7 @@ def bm file
     print file, "\t"
     run_command "ruby #{file}"
     print "\t"
-    run_command "java -jar xruby-0.1.1.jar #{file}"
+    run_command "java -jar xruby-0.1.2.jar #{file}"
     print "\n"
   end
 end
