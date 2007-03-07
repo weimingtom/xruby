e98462974594b10af3c54c6bc56b54974370206b
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-03-07 00:08:29 +0000

    Re-implement 'times', 'step' in Java to handle Fixnum as a special case (c ruby does it as well for performance reason)

--- src/com/xruby/runtime/builtin/IntegerClassBuilder.java
+++ src/com/xruby/runtime/builtin/IntegerClassBuilder.java
@@ -6,8 +6,7 @@
 package com.xruby.runtime.builtin;
 
 import com.xruby.runtime.lang.*;
-import com.xruby.runtime.value.RubyFixnum;
-import com.xruby.runtime.value.ObjectFactory;
+import com.xruby.runtime.value.*;
 
 class Integer_get_bit extends RubyOneArgMethod {
 	protected RubyValue run(RubyValue receiver, RubyValue arg, RubyBlock block)  {
@@ -34,10 +33,46 @@ class Integer_chr extends RubyNoArgMethod {
 	}
 }
 
+/*
+Without optimization for Fixnum, the code should look like:
+def times		
+	a = 0		
+	while a < self
+		yield a
+		a += 1; 	
+	end 
+end
+*/
+class Integer_times extends RubyNoArgMethod {
+	protected RubyValue run(RubyValue receiver, RubyBlock block) {
+		if (receiver instanceof RubyFixnum) {
+			int value = ((RubyFixnum)receiver).intValue();
+			for (int i = 0; i < value; ++i) {
+				RubyValue v = block.invoke(receiver, new RubyArray(ObjectFactory.createFixnum(i)));
+				if (block.breakedOrReturned()) {
+					return v;
+				}
+			}
+		} else {
+			RubyValue a = ObjectFactory.fixnum0;
+			while (RubyAPI.testTrueFalse(RubyAPI.callPublicOneArgMethod(a, receiver, null, "<"))) {
+				RubyValue v = block.invoke(receiver, new RubyArray(a));
+				if (block.breakedOrReturned()) {
+					return v;
+				}
+				a = RubyAPI.callPublicOneArgMethod(a, ObjectFactory.fixnum1, null, "+");
+			}
+		}
+		
+		return receiver;
+	}
+}
+
 public class IntegerClassBuilder {
 	public static void initialize() {
 		RubyClass c = RubyRuntime.IntegerClass;
 		c.defineMethod("[]",  new Integer_get_bit());
 		c.defineMethod("chr",  new Integer_chr());
+		c.defineMethod("times",  new Integer_times());
 	}
 }
