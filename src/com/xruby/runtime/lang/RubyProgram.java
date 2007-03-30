20de42eed7554f72d380a22e3318c79d3724d479
femtowin <femtowin@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-03-30 12:02:37 +0000

    refactoring ObjectFactory constant lower case to upper case

--- src/com/xruby/runtime/lang/RubyProgram.java
+++ src/com/xruby/runtime/lang/RubyProgram.java
@@ -1,22 +1,23 @@
-/** 
+/**
  * Copyright 2005-2007 Xue Yong Zhi
  * Distributed under the GNU General Public License 2.0
  */
 
 package com.xruby.runtime.lang;
 
-import com.xruby.runtime.value.*;
+import com.xruby.runtime.value.ObjectFactory;
+import com.xruby.runtime.value.RubyArray;
 
 public abstract class RubyProgram {
 
-	public RubyValue invoke() {
-		return invoke(ObjectFactory.topLevelSelfValue, null, null, RubyRuntime.ObjectClass);
-	}
+    public RubyValue invoke() {
+        return invoke(ObjectFactory.TOP_LEVEL_SELF_VALUE, null, null, RubyRuntime.ObjectClass);
+    }
 
-	public RubyValue invoke(RubyValue receiver, RubyArray args, RubyBlock block, RubyModule scope) {
-		RubyRuntime.ObjectClass.setAccessPrivate();/*so that top level methods are private methods of Object by default*/
-		return run(receiver, args, block, scope);
-	}
-	
-	protected abstract RubyValue run(RubyValue receiver, RubyArray args, RubyBlock block, RubyModule scope);
+    public RubyValue invoke(RubyValue receiver, RubyArray args, RubyBlock block, RubyModule scope) {
+        RubyRuntime.ObjectClass.setAccessPrivate();/*so that top level methods are private methods of Object by default*/
+        return run(receiver, args, block, scope);
+    }
+
+    protected abstract RubyValue run(RubyValue receiver, RubyArray args, RubyBlock block, RubyModule scope);
 }
