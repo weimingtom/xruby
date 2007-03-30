10391d8a7b9d94c85599b00b02b8ee1522c0fa33
femtowin <femtowin@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-03-30 12:12:08 +0000

    refactoring ObjectFactory constant lower case to upper case

--- src/com/xruby/runtime/lang/AtExitBlocks.java
+++ src/com/xruby/runtime/lang/AtExitBlocks.java
@@ -1,23 +1,24 @@
-/** 
+/**
  * Copyright 2005-2007 Xue Yong Zhi
  * Distributed under the GNU General Public License 2.0
  */
 
 package com.xruby.runtime.lang;
 
-import java.util.*;
-import com.xruby.runtime.value.*;
+import com.xruby.runtime.value.ObjectFactory;
+
+import java.util.ArrayList;
 
 public class AtExitBlocks {
-	private static ArrayList<RubyBlock> registgered_blocks_ = new ArrayList<RubyBlock>();
-	
-	public static void registerBlock(RubyBlock block) {
-		registgered_blocks_.add(0, block);
-	}
-	
-	static void invokeAll() {
-		for (RubyBlock block : registgered_blocks_) {
-			block.invoke(ObjectFactory.topLevelSelfValue, null);
-		}
-	}
+    private static ArrayList<RubyBlock> registgered_blocks_ = new ArrayList<RubyBlock>();
+
+    public static void registerBlock(RubyBlock block) {
+        registgered_blocks_.add(0, block);
+    }
+
+    static void invokeAll() {
+        for (RubyBlock block : registgered_blocks_) {
+            block.invoke(ObjectFactory.TOP_LEVEL_SELF_VALUE, null);
+        }
+    }
 }
