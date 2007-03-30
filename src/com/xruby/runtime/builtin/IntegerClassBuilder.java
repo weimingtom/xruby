0c5a47ed107165d8167a2a15ea922b6f29278114
femtowin <femtowin@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-03-30 11:55:50 +0000

    refactoring ObjectFactory constant lower case to upper case

--- src/com/xruby/runtime/builtin/IntegerClassBuilder.java
+++ src/com/xruby/runtime/builtin/IntegerClassBuilder.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * Copyright 2005-2007 Xue Yong Zhi, Jie Li, Ye Zheng
  * Distributed under the GNU General Public License 2.0
  */
@@ -6,31 +6,33 @@
 package com.xruby.runtime.builtin;
 
 import com.xruby.runtime.lang.*;
-import com.xruby.runtime.value.*;
+import com.xruby.runtime.value.ObjectFactory;
+import com.xruby.runtime.value.RubyArray;
+import com.xruby.runtime.value.RubyFixnum;
 
 class Integer_get_bit extends RubyOneArgMethod {
-	protected RubyValue run(RubyValue receiver, RubyValue arg, RubyBlock block)  {
-		int index = ((RubyFixnum)arg).intValue();
-		if (index < 0 || index >= 32){
-			return ObjectFactory.fixnum0;
-		}
-		int value = ((RubyFixnum)receiver).intValue();
-		if (((1 << index) & value) != 0) {
-			return ObjectFactory.fixnum1;
-		}
-		return ObjectFactory.fixnum0;
-	}
+    protected RubyValue run(RubyValue receiver, RubyValue arg, RubyBlock block) {
+        int index = ((RubyFixnum) arg).intValue();
+        if (index < 0 || index >= 32) {
+            return ObjectFactory.FIXNUM0;
+        }
+        int value = ((RubyFixnum) receiver).intValue();
+        if (((1 << index) & value) != 0) {
+            return ObjectFactory.FIXNUM1;
+        }
+        return ObjectFactory.FIXNUM0;
+    }
 }
 
 class Integer_chr extends RubyNoArgMethod {
-	protected RubyValue run(RubyValue receiver, RubyBlock block) {
-		int value = ((RubyFixnum)receiver).intValue();
-		if (value < 0 || value > 255){
-			throw new RubyException(RubyRuntime.RangeErrorClass, value + " out of char range");
-		}
-		char charValue = (char)value;
-		return ObjectFactory.createString("" + charValue);
-	}
+    protected RubyValue run(RubyValue receiver, RubyBlock block) {
+        int value = ((RubyFixnum) receiver).intValue();
+        if (value < 0 || value > 255) {
+            throw new RubyException(RubyRuntime.RangeErrorClass, value + " out of char range");
+        }
+        char charValue = (char) value;
+        return ObjectFactory.createString("" + charValue);
+    }
 }
 
 /*
@@ -44,35 +46,35 @@ def times
 end
 */
 class Integer_times extends RubyNoArgMethod {
-	protected RubyValue run(RubyValue receiver, RubyBlock block) {
-		if (receiver instanceof RubyFixnum) {
-			int value = ((RubyFixnum)receiver).intValue();
-			for (int i = 0; i < value; ++i) {
-				RubyValue v = block.invoke(receiver, new RubyArray(ObjectFactory.createFixnum(i)));
-				if (block.breakedOrReturned()) {
-					return v;
-				}
-			}
-		} else {
-			RubyValue a = ObjectFactory.fixnum0;
-			while (RubyAPI.testTrueFalse(RubyAPI.callPublicOneArgMethod(a, receiver, null, "<"))) {
-				RubyValue v = block.invoke(receiver, new RubyArray(a));
-				if (block.breakedOrReturned()) {
-					return v;
-				}
-				a = RubyAPI.callPublicOneArgMethod(a, ObjectFactory.fixnum1, null, "+");
-			}
-		}
-		
-		return receiver;
-	}
+    protected RubyValue run(RubyValue receiver, RubyBlock block) {
+        if (receiver instanceof RubyFixnum) {
+            int value = ((RubyFixnum) receiver).intValue();
+            for (int i = 0; i < value; ++i) {
+                RubyValue v = block.invoke(receiver, new RubyArray(ObjectFactory.createFixnum(i)));
+                if (block.breakedOrReturned()) {
+                    return v;
+                }
+            }
+        } else {
+            RubyValue a = ObjectFactory.FIXNUM0;
+            while (RubyAPI.testTrueFalse(RubyAPI.callPublicOneArgMethod(a, receiver, null, "<"))) {
+                RubyValue v = block.invoke(receiver, new RubyArray(a));
+                if (block.breakedOrReturned()) {
+                    return v;
+                }
+                a = RubyAPI.callPublicOneArgMethod(a, ObjectFactory.FIXNUM1, null, "+");
+            }
+        }
+
+        return receiver;
+    }
 }
 
 public class IntegerClassBuilder {
-	public static void initialize() {
-		RubyClass c = RubyRuntime.IntegerClass;
-		c.defineMethod("[]",  new Integer_get_bit());
-		c.defineMethod("chr",  new Integer_chr());
-		c.defineMethod("times",  new Integer_times());
-	}
+    public static void initialize() {
+        RubyClass c = RubyRuntime.IntegerClass;
+        c.defineMethod("[]", new Integer_get_bit());
+        c.defineMethod("chr", new Integer_chr());
+        c.defineMethod("times", new Integer_times());
+    }
 }
