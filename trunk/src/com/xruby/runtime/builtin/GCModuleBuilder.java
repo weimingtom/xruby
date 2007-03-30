9cefc3caf9b12f09e3ce84679757eda86972f5eb
femtowin <femtowin@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-03-30 11:57:07 +0000

    refactoring ObjectFactory constant lower case to upper case

--- trunk/src/com/xruby/runtime/builtin/GCModuleBuilder.java
+++ trunk/src/com/xruby/runtime/builtin/GCModuleBuilder.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * Copyright 2005-2007 Xue Yong Zhi, Ye Zheng
  * Distributed under the GNU General Public License 2.0
  */
@@ -6,18 +6,18 @@
 package com.xruby.runtime.builtin;
 
 import com.xruby.runtime.lang.*;
-import com.xruby.runtime.value.*;
+import com.xruby.runtime.value.ObjectFactory;
 
-class GC_start extends RubyNoArgMethod {	
-	protected RubyValue run(RubyValue receiver, RubyBlock block) {
-		System.gc();
-		return ObjectFactory.nilValue;
-	}
+class GC_start extends RubyNoArgMethod {
+    protected RubyValue run(RubyValue receiver, RubyBlock block) {
+        System.gc();
+        return ObjectFactory.NIL_VALUE;
+    }
 }
 
 public class GCModuleBuilder {
-	public static void initialize() {
-		RubyModule m = RubyRuntime.GCModule;
-		m.getSingletonClass().defineMethod("start", new GC_start());
-	}
+    public static void initialize() {
+        RubyModule m = RubyRuntime.GCModule;
+        m.getSingletonClass().defineMethod("start", new GC_start());
+    }
 }
