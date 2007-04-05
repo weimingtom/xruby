7167d454639938a5a99309563e8f87cbd278f683
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-04-05 21:55:05 +0000

    should pop up the value on the stack after bock parameters were initialized

--- src/com/xruby/compiler/codedom/BlockTest.java
+++ src/com/xruby/compiler/codedom/BlockTest.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * Copyright 2005-2007 Xue Yong Zhi
  * Distributed under the GNU General Public License 2.0
  */
@@ -9,8 +9,8 @@ package com.xruby.compiler.codedom;
 public class BlockTest extends TestingAstTestCase {
 	public void test_parameter() {
 		String program_text = "f {|a, b, c|   puts a  }";
-		
-		String expected_result = 
+
+		String expected_result =
 			"self\n" +
 			"block:3:false:0\n" +
 			"MultipleAssignment:false:true\n" +
@@ -20,6 +20,7 @@ public class BlockTest extends TestingAstTestCase {
 			"c //=\n" +
 			"b //=\n" +
 			"a //=\n" +
+			";\n" +
 			"self\n" +
 			"a\n" +
 			"puts:false\n" +
@@ -28,14 +29,15 @@ public class BlockTest extends TestingAstTestCase {
 			"EOF";
 		assertAstOutput(program_text, expected_result);
 	}
-	
+
 	public void test_dot_time() {
 		String program_text = "3.times {print 'Ho!'}";
-		
-		String expected_result = 
+
+		String expected_result =
 			"3\n" +
 			"block:-1:false:0\n" +
 			"MultipleAssignment:false:false\n" +
+			";\n" +
 			"self\n" +
 			"Ho!\n" +
 			"print:false\n" +
@@ -44,11 +46,11 @@ public class BlockTest extends TestingAstTestCase {
 			"EOF";
 		assertAstOutput(program_text, expected_result);
 	}
-	
+
 	public void test_asterisk_parameter() {
 		String program_text = "f {|a, b, *c|   puts c  }";
-		
-		String expected_result = 
+
+		String expected_result =
 			"self\n" +
 			"block:2:true:0\n" +
 			"MultipleAssignment:false:true\n" +
@@ -58,6 +60,7 @@ public class BlockTest extends TestingAstTestCase {
 			"c //=\n" +
 			"b //=\n" +
 			"a //=\n" +
+			";\n" +
 			"self\n" +
 			"c\n" +
 			"puts:false\n" +
@@ -66,15 +69,16 @@ public class BlockTest extends TestingAstTestCase {
 			"EOF";
 		assertAstOutput(program_text, expected_result);
 	}
-	
+
 	public void test_asterisk_parameter_no_name() {
 		String program_text = "f {|a, *|   puts a  }";
-		
-		String expected_result = 
+
+		String expected_result =
 			"self\n" +
 			"block:1:false:0\n" +
 			"MultipleAssignment:true:true\n" +
 			"a //=\n" +
+			";\n" +
 			"self\n" +
 			"a\n" +
 			"puts:false\n" +
@@ -83,14 +87,15 @@ public class BlockTest extends TestingAstTestCase {
 			"EOF";
 		assertAstOutput(program_text, expected_result);
 	}
-	
+
 	public void test_break_in_block() {
 		String program_text = "loop do break; end";
-		
-		String expected_result = 
+
+		String expected_result =
 			"self\n" +
 			"block:-1:false:0\n" +
 			"MultipleAssignment:false:false\n" +
+			";\n" +
 			"nil\n" +
 			"break\n" +
 			"end block:false\n" +
@@ -98,14 +103,14 @@ public class BlockTest extends TestingAstTestCase {
 			"EOF";
 		assertAstOutput(program_text, expected_result);
 	}
-	
+
 	public void test_do_end_is_same_as_curly_braces() {
 		String program_text1 = "f {|a, b, *c|   puts c  }";
-		
+
 		String program_text2 = "f do |a, b, *c|   puts c  end";
-		
+
 		assertAstEquals(program_text1, program_text2);
 	}
-	
-	
+
+
 }
