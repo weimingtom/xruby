81b7e5122ef6192f994bee3c33b463e5efdb30ef
beanworms <beanworms@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-04-10 07:02:27 +0000

    Add smap file

--- branches/debugger/src/com/xruby/compiler/codedom/Block.java
+++ branches/debugger/src/com/xruby/compiler/codedom/Block.java
@@ -5,16 +5,19 @@
 
 package com.xruby.compiler.codedom;
 
-import java.util.ArrayList;
+import java.util.*;
 
 class Pair {
 	String name;
 	String[] value;
-};
+} 
 
 public class Block {
+    // TODO: fix this cache(map), maybe it has a naming conflict in multiple files compile
+    private static Map<String, Map<String, int[]>> blockMap =
+            new HashMap<String, Map<String, int[]>>();
 
-	private CompoundStatement bodyStatement_ = null;
+    private CompoundStatement bodyStatement_ = null;
 	private ArrayList<ParameterVariableExpression> parameters_ = new ArrayList<ParameterVariableExpression>();
 	private ParameterVariableExpression asterisk_parameter_ = null;
 	private ArrayList<Expression> default_parameters_ = new ArrayList<Expression>();
@@ -22,6 +25,7 @@ public class Block {
 	private boolean is_for_in_expression_ = false;
 	private boolean has_extra_comma_ = false;
 
+    private String name;
     private int startPosition;
     private int endPosition;
 
@@ -85,7 +89,7 @@ public class Block {
 	}
 
 	public Pair accept(CodeVisitor visitor) {
-		String name = visitor.visitBlock((should_validate_argument_length_ ? parameters_.size() : -1),
+		name = visitor.visitBlock((should_validate_argument_length_ ? parameters_.size() : -1),
 									(null != asterisk_parameter_),
 									default_parameters_.size(),
 									is_for_in_expression_);
@@ -98,12 +102,15 @@ public class Block {
 			bodyStatement_.accept(visitor);
             setEndPosition(bodyStatement_.getLastLine());
         }
+        else {
+            setEndPosition(startPosition);
+        }
 
-		Pair p = new Pair();
+        Pair p = new Pair();
 		p.name = name;
 		p.value = visitor.visitBlockEnd(name, (null != bodyStatement_) ?
 										bodyStatement_.lastStatementHasReturnValue() : false);
-
+        Block.markBlock(this);
         return p;
 	}
 
@@ -120,7 +127,118 @@ public class Block {
         return endPosition;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setEndPosition(int endPosition) {
         this.endPosition = endPosition;
+    }
+
+    /**
+     * Return the block description for all blocks
+     *
+     * e.g.
+     * 2                       # Number of files
+     * a_script.rb             # script's file name
+     * 2                       # Number of blocks
+     * BLOCK$0 2 10            # block name start end
+     * BLOCK$1 22 30
+     * b_script.rb
+     * 2
+     * BLOCK$0 2 10
+     * BLOCK$1 22 30
+     *
+     * @return block description
+     */
+    public static String getBlockMap() {
+        StringBuffer description = new StringBuffer();
+        Set<String> files = blockMap.keySet();
+        description.append(files.size()).append("\n"); // Number of files
+
+        for(String scriptName: files) {
+            Map<String, int[]> map = blockMap.get(scriptName);
+            description.append(scriptName).append("\n"); // script's file name
+            description.append(map.size());  // Number of blocks
+
+            Set<String> blocks = map.keySet();
+            for(String blockName: blocks) {
+                int[] range = map.get(blockName);
+                String line = String.format("%s %d %d\n", blockName, range[0], range[1]);
+                description.append(line);
+            }
+        }
+
+        return description.toString();
+    }
+
+    /**
+     * 2                       # Number of blocks
+     * BLOCK$0 2 10            # block name start end
+     * BLOCK$1 22 30
+     * b_script.rb
+     *
+     * @param scriptName script file's name
+     * @return block map, string form
+     */
+    public static String getBlockMapByName(String scriptName) {
+        Set<String> files = blockMap.keySet();
+        if (!files.contains(scriptName)) {
+            return null;
+        } else {
+            StringBuffer description = new StringBuffer();
+            Map<String, int[]> map = blockMap.get(scriptName);
+            description.append(map.size()).append("\n");  // Number of blocks
+
+            Set<String> blocks = map.keySet();
+            for (String blockName : blocks) {
+                int[] range = map.get(blockName);
+                String line = String.format("%s %d %d\n", blockName, range[0], range[1]);
+                description.append(line);
+            }
+
+            return description.toString();
+        }
+    }
+
+    protected static void markBlock(Block block) {
+        String blockName = block.getName();
+        String name = extractBlockName(blockName);
+        String scriptName = extractScriptName(blockName);
+
+        Map<String, int[]> map = blockMap.get(scriptName);
+
+        // First block in this file
+        if(map == null) {
+            map = new HashMap<String, int[]>();
+            blockMap.put(scriptName, map);
+        }
+
+        int start = block.getStartPosition();
+        int end = block.getEndPosition();
+        int[] range = new int[] {start, end};
+        map.put(name, range);
+    }
+
+    private static String extractScriptName(String blockName) {
+        StringTokenizer st = new StringTokenizer(blockName, "/");
+
+        // TODO: maybe we need create a global variable to keep current script name
+        // TODO: Add a new global helper method getCurrentScriptName(). We need fix this in the feature.
+        if(st.hasMoreTokens()) {
+            return st.nextToken() + ".rb";
+        } else {
+            return null;
+        }
+    }
+
+    private static String extractBlockName(String name) {
+        StringTokenizer st = new StringTokenizer(name, "/");
+        String blockName = null;
+        while(st.hasMoreTokens()) {
+            blockName = st.nextToken();
+        }
+
+        return blockName;
     }
 }
