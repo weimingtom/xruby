611ecc748ae2faa6cad32166ac6273a65242ef68
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-04-11 00:25:49 +0000

    add RubySymbol.equal and RubySymbol.hashCode so that they can be used as key of hash table

--- trunk/src/com/xruby/runtime/value/RubySymbol.java
+++ trunk/src/com/xruby/runtime/value/RubySymbol.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * Copyright 2005-2007 Xue Yong Zhi
  * Distributed under the GNU General Public License 2.0
  */
@@ -7,17 +7,35 @@ package com.xruby.runtime.value;
 
 import com.xruby.runtime.lang.*;
 
+/**
+ * TODO right now RuySymbol does not behave like c ruby.
+ * Two ':symbol' may be two diffrence instances.
+ */
 public class RubySymbol extends RubyBasic {
 
 	private String value_;
-	
+
 	public RubySymbol(String s) {
 		super(RubyRuntime.SymbolClass);
 		value_ = s.intern();
 	}
-	
+
 	public String toString() {
 		return value_;
+	}
+
+	public boolean equals(Object obj) {
+		if (!(obj instanceof RubySymbol)) {
+			return false;
+		} else if (value_.equals(((RubySymbol)obj).value_)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int hashCode() {
+		return value_.hashCode();
 	}
 
 }
