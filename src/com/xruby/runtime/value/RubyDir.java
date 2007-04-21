e86dbf6d98f386c50835131e7793d184df175997
zhangyu8374 <zhangyu8374@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-04-21 15:36:26 +0000

    add close and read method

--- src/com/xruby/runtime/value/RubyDir.java
+++ src/com/xruby/runtime/value/RubyDir.java
@@ -6,20 +6,51 @@
 package com.xruby.runtime.value;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.xruby.runtime.lang.RubyBasic;
 import com.xruby.runtime.lang.RubyRuntime;
 
 public class RubyDir extends RubyBasic{
     private File dir_;
+    private boolean isOpen = true;
+    private int curPos;
+    private List<String> list = new ArrayList<String>();
     
     public RubyDir(String path){
         super(RubyRuntime.DirClass);
         dir_ = new File(path);
+        
+        list.add(".");
+        list.add("..");
+        String[] contents = dir_.list();
+        if(contents != null){
+            for(int i=0;i<contents.length;i++){
+                list.add(contents[i]);
+            }
+        }
+        curPos = 0;
     }
     
     public boolean isDirectory(){
         return dir_.isDirectory();
     }
     
+    public void close(){
+        isOpen = false;
+    }
+    
+    public boolean isOpen(){
+        return isOpen;
+    }
+    
+    public String read(){
+        if(curPos >= list.size())
+            return null;
+        
+        String tmp = list.get(curPos);
+        curPos++;
+        return tmp;
+    }
 }
