8ede351231d5d31f0077d8cbcc874a47756a51b1
femtowin <femtowin@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-05-03 08:14:57 +0000

    string_escape, next step, handle string substitution and '\'', "\"" ,as well as error reporting

--- branches/antlrv3/src/com/xruby/compiler/codedom/DoubleQuoteStringExpression.java
+++ branches/antlrv3/src/com/xruby/compiler/codedom/DoubleQuoteStringExpression.java
@@ -1,23 +1,160 @@
 package com.xruby.compiler.codedom;
 
+import com.xruby.compiler.parser.SyntaxException;
+
 /**
  * Copyright 2005-2007 femto
  * Distributed under the GNU General Public License 2.0
  */
-public class DoubleQuoteStringExpression extends StringExpression {
-    private String value;
+public class DoubleQuoteStringExpression extends StringExpressionWithExpressionSubstitution {
+
 
     public DoubleQuoteStringExpression(String value) {
+
+        String text = null;
         if (value.startsWith("\"")) {
-            this.value = value.substring(1, value.length() - 1);
+            text = value.substring(1, value.length() - 1);
         } else if (value.startsWith("%Q")) {
-            this.value = value.substring(3, value.length() - 1);
+            text = value.substring(3, value.length() - 1);
         } else if (value.startsWith("%")) {
-            this.value = value.substring(2, value.length() - 1);
+            text = value.substring(2, value.length() - 1);
         }
+        parseString(text);
     }
 
-    public void accept(CodeVisitor visitor) {
-        visitor.visitStringExpression(value.toString());
+    private void parseString(String text) {
+        StringBuffer tokens = new StringBuffer();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c == '\\') {
+                if (i == text.length() - 1) {
+                    throw new SyntaxException("unterminated string meets end of file");
+                }
+                i++;
+                //c = text.charAt(i);
+                i = read_escape(text, i, tokens);
+            } else if (c == '#' && i < text.length() - 1) {
+                i++;
+                c = text.charAt(i);
+                if (c == '{') {
+                    //read_statements();
+                } //todo: more on this c=='@', c=='$'
+            } else {
+                tokens.append(c);
+            }
+        }
+        stmts_.add(tokens.toString());
     }
+
+    //port from JRuby
+    /**
+     * @param text
+     * @param index
+     * @param tokens
+     * @return new Index after we read escape
+     */
+    private int read_escape(String text, int index, StringBuffer tokens) {
+        char c;
+        switch (c = text.charAt(index)) {
+            case 'n':
+                tokens.append('\n');
+                return ++index;
+            case 'r':
+                tokens.append('\r');
+                return ++index;
+            case '\\': // backslash
+                tokens.append('\\');
+                return ++index;
+            case 't': // horizontal tab
+                tokens.append('\t');
+                return ++index;
+            case 'f': // form feed
+                tokens.append('\f');
+                return ++index;
+            case 'v': // vertical tab
+                tokens.append('\u0013');
+                return ++index;
+            case 'a': // alarm(bell)
+                tokens.append('\u0007');
+                return ++index;
+            case 'e': // escape
+                tokens.append('\u0033');
+                return ++index;
+            case 'b': // backspace
+                tokens.append('\010');
+                return ++index;
+            case 's': // space
+                tokens.append(' ');
+                return ++index;
+            case '0':
+            case '1':
+            case '2':
+            case '3': // octal constant
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+                return scanOct(text, index, tokens, 3);
+            case 'x':
+                return scanHex(text, index + 1, tokens, 2);
+            default:
+                tokens.append(c);
+                return ++index;
+        }
+
+    }
+
+    private int scanOct(String text, int index, StringBuffer tokens, int count) {
+        char value = '\0';
+
+        for (int i = 0; i < count; i++) {
+            if (index == text.length()) {
+                break;
+            }
+            char c = text.charAt(index++);
+
+            if (!isOctChar(c)) {
+                index--;
+                break;
+            }
+
+            value <<= 3;
+            value |= Integer.parseInt("" + c, 8);
+        }
+        if (value > 255) {
+            value = 255;
+        }
+        tokens.append(value);
+        return index;
+    }
+
+    private int scanHex(String text, int index, StringBuffer tokens, int count) {
+        char value = '\0';
+
+        for (int i = 0; i < count; i++) {
+            if (index == text.length()) {
+                break;
+            }
+            char c = text.charAt(index++);
+
+            if (!isHexChar(c)) {
+                index--;
+                break;
+            }
+
+            value <<= 4;
+            value |= Integer.parseInt("" + c, 16) & 15;
+        }
+        tokens.append(value);
+        return index;
+    }
+
+    private boolean isHexChar(char c) {
+        return Character.isDigit(c) || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F');
+    }
+
+    private boolean isOctChar(char c) {
+        return '0' <= c && c <= '7';
+    }
+
 }
--- branches/antlrv3/src/com/xruby/compiler/codedom/StringExpressionWithExpressionSubstitution.java
+++ branches/antlrv3/src/com/xruby/compiler/codedom/StringExpressionWithExpressionSubstitution.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * Copyright 2005-2007 Xue Yong Zhi
  * Distributed under the GNU General Public License 2.0
  */
@@ -6,44 +6,47 @@
 package com.xruby.compiler.codedom;
 
 public class StringExpressionWithExpressionSubstitution extends ExpressionWithExpressionSubstitution {
-	
-	public StringExpressionWithExpressionSubstitution(String s) {
-		super.addString(StringExpression.convertDoubleQuoteString(s));
-	}
+    //for v3
+    public StringExpressionWithExpressionSubstitution() {
+    }
 
-	public void addString(String s) {
-		super.addString(StringExpression.convertDoubleQuoteString(s));
-	}
-	
-	void merge(StringExpressionWithExpressionSubstitution another) {
-		stmts_.addAll(another.stmts_);
-	}
+    public StringExpressionWithExpressionSubstitution(String s) {
+        super.addString(StringExpression.convertDoubleQuoteString(s));
+    }
 
-	public void accept(CodeVisitor visitor) {
-		
-		visitor.visitStringExpressionWithExpressionSubstitutionBegin();
-		
-		for (Object o : stmts_) {
-			if (o instanceof String) {
-				visitor.visitStringExpressionWithExpressionSubstitution((String)o);
-			} else if (o instanceof InstanceVariableExpression) {
-				((InstanceVariableExpression)o).accept(visitor);
-				visitor.visitStringExpressionWithExpressionSubstitution();
-			} else if (o instanceof ClassVariableExpression) {
-				((ClassVariableExpression)o).accept(visitor);
-				visitor.visitStringExpressionWithExpressionSubstitution();
-			} else if (o instanceof GlobalVariableExpression) {
-				((GlobalVariableExpression)o).accept(visitor);
-				visitor.visitStringExpressionWithExpressionSubstitution();
-			} else if (o instanceof CompoundStatement){
-				((CompoundStatement)o).accept(visitor);
-				visitor.visitStringExpressionWithExpressionSubstitution();
-			} else {
-				assert(false);
-			}
-		}
-		
-		visitor.visitStringExpressionWithExpressionSubstitutionEnd();
-	}
-	
+    public void addString(String s) {
+        super.addString(StringExpression.convertDoubleQuoteString(s));
+    }
+
+    void merge(StringExpressionWithExpressionSubstitution another) {
+        stmts_.addAll(another.stmts_);
+    }
+
+    public void accept(CodeVisitor visitor) {
+
+        visitor.visitStringExpressionWithExpressionSubstitutionBegin();
+
+        for (Object o : stmts_) {
+            if (o instanceof String) {
+                visitor.visitStringExpressionWithExpressionSubstitution((String) o);
+            } else if (o instanceof InstanceVariableExpression) {
+                ((InstanceVariableExpression) o).accept(visitor);
+                visitor.visitStringExpressionWithExpressionSubstitution();
+            } else if (o instanceof ClassVariableExpression) {
+                ((ClassVariableExpression) o).accept(visitor);
+                visitor.visitStringExpressionWithExpressionSubstitution();
+            } else if (o instanceof GlobalVariableExpression) {
+                ((GlobalVariableExpression) o).accept(visitor);
+                visitor.visitStringExpressionWithExpressionSubstitution();
+            } else if (o instanceof CompoundStatement) {
+                ((CompoundStatement) o).accept(visitor);
+                visitor.visitStringExpressionWithExpressionSubstitution();
+            } else {
+                assert (false);
+            }
+        }
+
+        visitor.visitStringExpressionWithExpressionSubstitutionEnd();
+    }
+
 }
