10a351a5aacb09372033545f541a074c625b31fa
zhangyu8374 <zhangyu8374@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-05-09 12:53:46 +0000

    add Thread.alive?,Thread#main and Thread#list

--- src/com/xruby/runtime/builtin/ThreadClassBuilder.java
+++ src/com/xruby/runtime/builtin/ThreadClassBuilder.java
@@ -1,11 +1,21 @@
 /**
- * Copyright 2005-2007 Xue Yong Zhi
+ * Copyright 2005-2007 Xue Yong Zhi, Yu Zhang
  * Distributed under the GNU General Public License 2.0
  */
 
 package com.xruby.runtime.builtin;
 
-import com.xruby.runtime.lang.*;
+import java.util.Iterator;
+import java.util.List;
+
+import com.xruby.runtime.lang.RubyBlock;
+import com.xruby.runtime.lang.RubyClass;
+import com.xruby.runtime.lang.RubyNoArgMethod;
+import com.xruby.runtime.lang.RubyOneArgMethod;
+import com.xruby.runtime.lang.RubyRuntime;
+import com.xruby.runtime.lang.RubyTwoArgMethod;
+import com.xruby.runtime.lang.RubyValue;
+import com.xruby.runtime.lang.RubyVarArgMethod;
 import com.xruby.runtime.value.ObjectFactory;
 import com.xruby.runtime.value.RubyArray;
 import com.xruby.runtime.value.RubyThread;
@@ -55,13 +65,46 @@ class Thread_array_set extends RubyTwoArgMethod {
     }
 }
 
+class Thread_list extends RubyNoArgMethod {
+    protected RubyValue run(RubyValue receiver, RubyBlock block) {
+        List list = RubyThread.getThreadList();
+        Iterator iter = list.iterator();
+        RubyArray result = new RubyArray(0);
+        while(iter.hasNext()){
+            result.add((RubyThread)iter.next());
+        }
+        return result;
+    }
+}
+
+class Thread_main extends RubyNoArgMethod {
+    protected RubyValue run(RubyValue receiver, RubyBlock block) {
+        return RubyThread.getMainThread();
+    }
+}
+
+class Thread_is_alive extends RubyNoArgMethod {
+    protected RubyValue run(RubyValue receiver, RubyBlock block) {
+        if(((RubyThread)receiver).isAlive()){
+            return ObjectFactory.TRUE_VALUE;
+        }else{
+            return ObjectFactory.FALSE_VALUE;
+        }
+    }
+}
+
 public class ThreadClassBuilder {
     public static void initialize() {
+        RubyThread.init();
+        
         RubyClass c = RubyRuntime.ThreadClass;
         c.defineMethod("join", new Thread_join());
         c.defineMethod("[]", new Thread_array_access());
         c.defineMethod("[]=", new Thread_array_set());
         c.defineMethod("==", new Thread_equal());
+        c.defineMethod("alive?", new Thread_is_alive());
+        c.getSingletonClass().defineMethod("main", new Thread_main());
+        c.getSingletonClass().defineMethod("list", new Thread_list());
         c.getSingletonClass().defineMethod("current", new Thread_current());
         c.getSingletonClass().defineMethod("new", new Thread_new());
     }
