04026827834c38215283efb42a3a912fca8b79c3
zhangyu8374 <zhangyu8374@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-05-09 12:51:58 +0000

    some modifications to make Thread work correctly.

--- trunk/src/com/xruby/runtime/value/RubyThread.java
+++ trunk/src/com/xruby/runtime/value/RubyThread.java
@@ -1,16 +1,19 @@
 /**
- * Copyright 2005-2007 Xue Yong Zhi
+ * Copyright 2005-2007 Xue Yong Zhi, Yu Zhang
  * Distributed under the GNU General Public License 2.0
  */
 
 package com.xruby.runtime.value;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import com.xruby.runtime.lang.RubyBasic;
 import com.xruby.runtime.lang.RubyBlock;
 import com.xruby.runtime.lang.RubyRuntime;
 import com.xruby.runtime.lang.RubyValue;
-import com.xruby.runtime.lang.RubyBasic;
-
-import java.util.HashMap;
 
 public class RubyThread extends RubyBasic {
 
@@ -21,20 +24,39 @@ public class RubyThread extends RubyBasic {
                     return new HashMap<String, RubyValue>();
                 }
             };
-
+    private RubyThreadGroup threadGroup;
+    private static List<RubyThread> activeThreads = new Vector<RubyThread>();
+    private static Map<Thread, RubyThread> threadMapper = new HashMap();
+    private static RubyThread mainThread;
+    
     RubyThread(final RubyBlock block) {
         super(RubyRuntime.ThreadClass);
         thread_ = new Thread(new Runnable() {
             public void run() {
+                RubyThreadGroup.defaultThreadGroup.add(RubyThread.this);
+                activeThreads.add(RubyThread.this);
                 block.invoke(null, null);
+                activeThreads.remove(RubyThread.this);
+                RubyThreadGroup.defaultThreadGroup.add(RubyThread.this);
+                threadMapper.remove(RubyThread.this.thread_);
             }
         });
         thread_.start();
+        threadMapper.put(thread_, this);
     }
-
+    
     private RubyThread(Thread thread) {
         super(RubyRuntime.ThreadClass);
         thread_ = thread;
+        threadMapper.put(thread_, this);
+    }
+    
+    //Only called from <code>ThreadClassBuilder</code> to add main thread.
+    public static void init(){
+        //Assume that this method is called from main thread
+        mainThread = new RubyThread(Thread.currentThread());
+        activeThreads.add(mainThread);
+        RubyThreadGroup.defaultThreadGroup.add(mainThread);
     }
 
     public boolean equals(Object o) {
@@ -56,7 +78,7 @@ public class RubyThread extends RubyBasic {
     }
 
     public static RubyThread current() {
-        return new RubyThread(Thread.currentThread());
+        return threadMapper.get(Thread.currentThread());
     }
 
     public static RubyValue getVariable(String name) {
@@ -72,5 +94,27 @@ public class RubyThread extends RubyBasic {
         thread_local_variables_.get().put(name, value);
         return value;
     }
-
+    
+    public RubyValue getThreadGroup(){
+        if(threadGroup == null)
+            return ObjectFactory.NIL_VALUE;
+        else
+            return threadGroup;
+    }
+    
+    public void setThreadGroup(RubyThreadGroup group){
+        threadGroup = group;
+    }  
+    
+    public static List getThreadList(){
+        return activeThreads;
+    }
+    
+    public boolean isAlive(){
+        return thread_.isAlive();
+    }
+    
+    public static RubyValue getMainThread(){
+        return mainThread;
+    }
 }
