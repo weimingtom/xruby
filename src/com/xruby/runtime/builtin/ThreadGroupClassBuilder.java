bd215e306a84e38a14a5b8ea960d26ea906d0341
zhangyu8374 <zhangyu8374@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-05-09 12:54:44 +0000

    add ThreadGroupClassBuilder

---
+++ src/com/xruby/runtime/builtin/ThreadGroupClassBuilder.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2005-2007 Yu Zhang
+ * Distributed under the GNU General Public License 2.0
+ */
+
+package com.xruby.runtime.builtin;
+
+import java.util.Iterator;
+import java.util.List;
+
+import com.xruby.runtime.lang.RubyBlock;
+import com.xruby.runtime.lang.RubyClass;
+import com.xruby.runtime.lang.RubyNoArgMethod;
+import com.xruby.runtime.lang.RubyOneArgMethod;
+import com.xruby.runtime.lang.RubyRuntime;
+import com.xruby.runtime.lang.RubyValue;
+import com.xruby.runtime.value.RubyArray;
+import com.xruby.runtime.value.RubyThread;
+import com.xruby.runtime.value.RubyThreadGroup;
+
+class ThreadGroup_list extends RubyNoArgMethod {
+    protected RubyValue run(RubyValue receiver, RubyBlock block) {
+        RubyThreadGroup group = (RubyThreadGroup)receiver;
+        List list = group.list();
+        Iterator iter = list.iterator();
+        RubyArray result = new RubyArray(0);
+        while(iter.hasNext()){
+            result.add((RubyThread)iter.next());
+        }
+        return result;
+    }
+}
+
+class ThreadGroup_new extends RubyNoArgMethod {
+    protected RubyValue run(RubyValue receiver, RubyBlock block) {
+        return new RubyThreadGroup();
+    }
+}
+
+class ThreadGroup_add extends RubyOneArgMethod {
+    protected RubyValue run(RubyValue receiver, RubyValue arg, RubyBlock block) {
+        RubyThread thread = (RubyThread)arg;
+        ((RubyThreadGroup)receiver).add(thread);
+        return receiver;
+    }
+}
+
+public class ThreadGroupClassBuilder {
+    public static void initialize() {
+        RubyClass c = RubyRuntime.ThreadGroupClass;             
+        c.defineMethod("add", new ThreadGroup_add());
+        c.defineMethod("list", new ThreadGroup_list());      
+        
+        c.setConstant("Default",RubyThreadGroup.defaultThreadGroup);
+        c.getSingletonClass().defineMethod("new", new ThreadGroup_new());
+    }
+}
