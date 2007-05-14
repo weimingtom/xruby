ff8b5673af76ab5e16b514ad3c844f5e6a3f619a
zhangyu8374 <zhangyu8374@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-05-14 02:21:06 +0000

    add some methods for Thread.

--- trunk/src/com/xruby/runtime/value/RubyThread.java
+++ trunk/src/com/xruby/runtime/value/RubyThread.java
@@ -8,7 +8,9 @@ package com.xruby.runtime.value;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
+import java.util.concurrent.locks.ReentrantLock;
 
 import com.xruby.runtime.lang.RubyBasic;
 import com.xruby.runtime.lang.RubyBlock;
@@ -18,7 +20,7 @@ import com.xruby.runtime.lang.RubyValue;
 public class RubyThread extends RubyBasic {
 
     private final Thread thread_;
-    private static ThreadLocal<HashMap<String, RubyValue>> thread_local_variables_ =
+    private ThreadLocal<HashMap<String, RubyValue>> thread_local_variables_ =
             new ThreadLocal<HashMap<String, RubyValue>>() {
                 protected synchronized HashMap<String, RubyValue> initialValue() {
                     return new HashMap<String, RubyValue>();
@@ -26,8 +28,17 @@ public class RubyThread extends RubyBasic {
             };
     private RubyThreadGroup threadGroup;
     private static List<RubyThread> activeThreads = new Vector<RubyThread>();
-    private static Map<Thread, RubyThread> threadMapper = new HashMap();
+    private static Map<Thread, RubyThread> threadMapper = new HashMap<Thread, RubyThread>();
     private static RubyThread mainThread;
+    
+    private ReentrantLock lock = new ReentrantLock();
+    private volatile boolean killed = false;
+    private static volatile boolean stoped = false;
+    
+    public static final Object stopLock = new Object();
+    public static final ReentrantLock criticalLock = new ReentrantLock();
+    
+    private RubyValue result = ObjectFactory.NIL_VALUE;
     
     RubyThread(final RubyBlock block) {
         super(RubyRuntime.ThreadClass);
@@ -35,12 +46,13 @@ public class RubyThread extends RubyBasic {
             public void run() {
                 RubyThreadGroup.defaultThreadGroup.add(RubyThread.this);
                 activeThreads.add(RubyThread.this);
-                block.invoke(null, null);
+                result = block.invoke(null, null);
                 activeThreads.remove(RubyThread.this);
                 RubyThreadGroup.defaultThreadGroup.add(RubyThread.this);
                 threadMapper.remove(RubyThread.this.thread_);
             }
         });
+        thread_.setDaemon(true);
         thread_.start();
         threadMapper.put(thread_, this);
     }
@@ -69,19 +81,23 @@ public class RubyThread extends RubyBasic {
         }
     }
 
-    public void join() {
+    public void join(long timeout) {
         try {
-            thread_.join();
+            thread_.join(timeout);
         } catch (InterruptedException e) {
             throw new Error(e);
         }
+    }
+    
+    public boolean isCurrentThread(){
+        return thread_ == Thread.currentThread();
     }
 
     public static RubyThread current() {
         return threadMapper.get(Thread.currentThread());
     }
 
-    public static RubyValue getVariable(String name) {
+    public RubyValue getVariable(String name) {
         RubyValue v = thread_local_variables_.get().get(name);
         if (null == v) {
             return ObjectFactory.NIL_VALUE;
@@ -90,9 +106,13 @@ public class RubyThread extends RubyBasic {
         return v;
     }
 
-    public static RubyValue setVariable(String name, RubyValue value) {
+    public RubyValue setVariable(String name, RubyValue value) {
         thread_local_variables_.get().put(name, value);
         return value;
+    }
+    
+    public Set getVariables(){
+        return thread_local_variables_.get().keySet();
     }
     
     public RubyValue getThreadGroup(){
@@ -110,11 +130,63 @@ public class RubyThread extends RubyBasic {
         return activeThreads;
     }
     
-    public boolean isAlive(){
-        return thread_.isAlive();
-    }
-    
     public static RubyValue getMainThread(){
         return mainThread;
     }
+    
+    public Thread getNativeThread(){
+        return thread_;
+    }
+    
+    public void setKilled(){
+        lock.lock();        
+        try{
+            killed = true;
+        }finally{
+            lock.unlock();
+        }        
+    }
+    
+    public boolean getKilled(){
+        return killed;
+    }
+    
+    public static void setStoped(boolean value){
+        stoped = value;
+    }
+    
+    public boolean getStoped(){
+        return stoped;
+    }
+    
+    public static void setCritical(boolean critical){
+        if (RubyThread.criticalLock.isHeldByCurrentThread()) {
+            if (!critical) {
+                RubyThread.criticalLock.unlock();
+            }
+        } else {
+            if (critical) {
+                RubyThread.criticalLock.lock();
+            }
+        }
+    }
+    
+    public RubyValue getResult(){
+        return result;
+    }
+    
+    //Invoked by KernelModuleBuilder's sleep.
+    public static void sleep(long milliseconds){
+        synchronized(stopLock){
+            stoped = true;
+            try {
+                stopLock.wait(milliseconds);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally{
+                stoped = false;
+            }
+        }
+    }
+    
 }
