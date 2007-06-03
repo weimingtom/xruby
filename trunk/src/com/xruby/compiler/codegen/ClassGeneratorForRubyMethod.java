bd0c0ec6004e13c7a0a92d3418f00e01dbf8de25
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-06-03 20:39:06 +0000

    tab->space

--- trunk/src/com/xruby/compiler/codegen/ClassGeneratorForRubyMethod.java
+++ trunk/src/com/xruby/compiler/codegen/ClassGeneratorForRubyMethod.java
@@ -11,134 +11,135 @@ import org.objectweb.asm.commons.Method;
 
 class ClassGeneratorForRubyMethod extends ClassGenerator {
 
-	private final String method_name_;//this name is saved for 'super'
-	private final boolean is_singleton_method_;
-	private final boolean has_only_one_arg_;
-    private String fileName;
+    private final String method_name_;//this name is saved for 'super'
+    private final boolean is_singleton_method_;
+    private final boolean has_only_one_arg_;
 
     public ClassGeneratorForRubyMethod(String method_name, String fileName, String name, int argc, boolean has_asterisk_parameter, int default_argc, boolean is_singleton_method) {
-		super(name);
-		method_name_ = method_name;
-        this.fileName = fileName;
+        super(name);
+        method_name_ = method_name;
         is_singleton_method_ = is_singleton_method;
-		has_only_one_arg_ = ((1 == argc) && !has_asterisk_parameter && (0 == default_argc));
-		if (has_only_one_arg_) {
-			mg_for_run_method_ = visitRubyOneArgMethod();
-		} else {
-			mg_for_run_method_ = visitRubyVarArgMethod(argc, has_asterisk_parameter, default_argc);
-		}
+        has_only_one_arg_ = ((1 == argc) && !has_asterisk_parameter && (0 == default_argc));
+        if (has_only_one_arg_) {
+            mg_for_run_method_ = visitRubyOneArgMethod();
+        } else {
+            mg_for_run_method_ = visitRubyVarArgMethod(argc, has_asterisk_parameter, default_argc);
+        }
 
         // set source file's name, for debug
-        if(fileName != null) {
+        if (null != fileName) {
             cv_.visitSource(fileName, null);
+        }
+
+
+    }
+
+    public boolean hasOnlyOneArg() {
+        return has_only_one_arg_;
+    }
+
+    public void loadMethodPrameter(int index) {
+        //signatiure:
+        //run(RubyValue reciever, RubyArray args, RubyBlock block)
+        //or
+        //run(RubyValue reciever, RubyValue arg, RubyBlock block)
+        if (has_only_one_arg_) {
+            assert(0 == index);
+            getMethodGenerator().loadArg(1);
+        } else {
+            super.loadMethodPrameter(index);
         }
     }
 
-	public boolean hasOnlyOneArg() {
-		return has_only_one_arg_;
-	}
+    public void storeMethodParameter(int index) {
+        if (has_only_one_arg_) {
+            assert(0 == index);
+            getMethodGenerator().storeArg(1);
+        } else {
+            super.storeMethodParameter(index);
+        }
+    }
 
-	public void loadMethodPrameter(int index) {
-		//signatiure:
-		//run(RubyValue reciever, RubyArray args, RubyBlock block)
-		//or
-		//run(RubyValue reciever, RubyValue arg, RubyBlock block)
-		if (has_only_one_arg_) {
-			assert(0 == index);
-			getMethodGenerator().loadArg(1);
-		} else {
-			super.loadMethodPrameter(index);
-		}
-	}
+    String getMethodName() {
+        return method_name_;
+    }
 
-	public void storeMethodParameter(int index) {
-		if (has_only_one_arg_) {
-			assert(0 == index);
-			getMethodGenerator().storeArg(1);
-		} else {
-			super.storeMethodParameter(index);
-		}
-	}
+    boolean isSingletonMethod() {
+        return is_singleton_method_;
+    }
 
-	String getMethodName() {
-		return method_name_;
-	}
+    protected Class getCurrentType() {
+        return Types.RubyMethodClass;
+    }
 
-	boolean isSingletonMethod() {
-		return is_singleton_method_;
-	}
-
-	protected Class getCurrentType() {
-		return Types.RubyMethodClass;
-	}
-
-	private MethodGenerator visitRubyVarArgMethod(int argc, boolean has_asterisk_parameter, int default_argc) {
-		cv_.visit(Opcodes.V1_5,
-				0,		//No modifier
-				name_,
-				null,								// signature
-				"com/xruby/runtime/lang/RubyVarArgMethod",	// superName
-				null								// interface
-				);
+    private MethodGenerator visitRubyVarArgMethod(int argc, boolean has_asterisk_parameter, int default_argc) {
+        cv_.visit(Opcodes.V1_5,
+                0,		//No modifier
+                name_,
+                null,								// signature
+                "com/xruby/runtime/lang/RubyVarArgMethod",	// superName
+                null								// interface
+                );
 
         createConstructorOfRubyVarArgMethod(argc, has_asterisk_parameter, default_argc);
 
-		return new MethodGenerator(Opcodes.ACC_PROTECTED,
-				Method.getMethod("com.xruby.runtime.lang.RubyValue run(com.xruby.runtime.lang.RubyValue, com.xruby.runtime.value.RubyArray, com.xruby.runtime.lang.RubyBlock)"),
-				cv_,
-				null,
-				null,
-				false);
-	}
+        return new MethodGenerator(Opcodes.ACC_PROTECTED,
+                Method.getMethod("com.xruby.runtime.lang.RubyValue run(com.xruby.runtime.lang.RubyValue, com.xruby.runtime.value.RubyArray, com.xruby.runtime.lang.RubyBlock)"),
+                cv_,
+                null,
+                null,
+                false);
+    }
 
-	private MethodGenerator visitRubyOneArgMethod() {
-				cv_.visit(Opcodes.V1_5,
-				0,		//No modifier
-				name_,
-				null,								// signature
-				"com/xruby/runtime/lang/RubyOneArgMethod",	// superName
-				null								// interface
-				);
+    private MethodGenerator visitRubyOneArgMethod() {
+                cv_.visit(Opcodes.V1_5,
+                0,		//No modifier
+                name_,
+                null,								// signature
+                "com/xruby/runtime/lang/RubyOneArgMethod",	// superName
+                null								// interface
+                );
 
-		createConstructorOfRubyOneArgMethod();
+        createConstructorOfRubyOneArgMethod();
 
-		return new MethodGenerator(Opcodes.ACC_PROTECTED,
-				Method.getMethod("com.xruby.runtime.lang.RubyValue run(com.xruby.runtime.lang.RubyValue, com.xruby.runtime.lang.RubyValue, com.xruby.runtime.lang.RubyBlock)"),
-				cv_,
-				null,
-				null,
-				false);
-	}
+        return new MethodGenerator(Opcodes.ACC_PROTECTED,
+                Method.getMethod("com.xruby.runtime.lang.RubyValue run(com.xruby.runtime.lang.RubyValue, com.xruby.runtime.lang.RubyValue, com.xruby.runtime.lang.RubyBlock)"),
+                cv_,
+                null,
+                null,
+                false);
+    }
 
-	private void createConstructorOfRubyVarArgMethod(int argc, boolean has_asterisk_parameter, int default_argc) {
-		MethodGenerator mg = new MethodGenerator(Opcodes.ACC_PUBLIC,
-				Method.getMethod("void <init> ()"),
-				cv_,
-				null,
-				null,
-				false);
-		mg.loadThis();
-		mg.push(argc);
-		mg.push(has_asterisk_parameter);
-		mg.push(default_argc);
-		mg.invokeConstructor(Type.getType(Types.RubyVarArgMethodClass),
-						Method.getMethod("void <init> (int, boolean, int)"));
-		mg.returnValue();
-		mg.endMethod();
-	}
+    private void createConstructorOfRubyVarArgMethod(int argc, boolean has_asterisk_parameter, int default_argc) {
+        MethodGenerator mg = new MethodGenerator(Opcodes.ACC_PUBLIC,
+                Method.getMethod("void <init> ()"),
+                cv_,
+                null,
+                null,
+                false);
+        mg.loadThis();
+        mg.push(argc);
+        mg.push(has_asterisk_parameter);
+        mg.push(default_argc);
+        mg.invokeConstructor(Type.getType(Types.RubyVarArgMethodClass),
+                        Method.getMethod("void <init> (int, boolean, int)"));
+        mg.returnValue();
+        mg.endMethod();
+    }
 
-	private void createConstructorOfRubyOneArgMethod() {
-		MethodGenerator mg = new MethodGenerator(Opcodes.ACC_PUBLIC,
-				Method.getMethod("void <init> ()"),
-				cv_,
-				null,
-				null,
-				false);
-		mg.loadThis();
-		mg.invokeConstructor(Type.getType(Types.RubyOneArgMethodClass),
-						Method.getMethod("void <init> ()"));
-		mg.returnValue();
-		mg.endMethod();
-	}
+    private void createConstructorOfRubyOneArgMethod() {
+        MethodGenerator mg = new MethodGenerator(Opcodes.ACC_PUBLIC,
+                Method.getMethod("void <init> ()"),
+                cv_,
+                null,
+                null,
+                false);
+        mg.loadThis();
+        mg.invokeConstructor(Type.getType(Types.RubyOneArgMethodClass),
+                        Method.getMethod("void <init> ()"));
+        mg.returnValue();
+        mg.endMethod();
+    }
+
 }
 
