c75a132ef60ff111bd6b1e0029f3e562f1b4638c
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-06-04 11:49:52 +0000

    add Proc#==

--- trunk/src/com/xruby/runtime/builtin/ProcClassBuilder.java
+++ trunk/src/com/xruby/runtime/builtin/ProcClassBuilder.java
@@ -9,49 +9,60 @@ import com.xruby.runtime.lang.*;
 import com.xruby.runtime.value.*;
 
 class Proc_call extends RubyVarArgMethod {
-	protected RubyValue run(RubyValue receiver, RubyArray args, RubyBlock block) {
-		throw new Error("we overided invoke, so this method should never be called");
-	}
+    protected RubyValue run(RubyValue receiver, RubyArray args, RubyBlock block) {
+        throw new Error("we overided invoke, so this method should never be called");
+    }
 
-	public RubyValue invoke(RubyValue receiver, RubyValue arg, RubyArray args, RubyBlock block) {
-		if (null != arg) {
-			assert(null == args);
-			args = ObjectFactory.createArray(1, 0, true);
-			args.add(arg);
-		}
+    public RubyValue invoke(RubyValue receiver, RubyValue arg, RubyArray args, RubyBlock block) {
+        if (null != arg) {
+            assert(null == args);
+            args = ObjectFactory.createArray(1, 0, true);
+            args.add(arg);
+        }
 
-		RubyValue v = ((RubyProc)receiver).call(args);
-		block = ((RubyProc)receiver).getBlock();
-		if (null != block) {
-			v.setReturnedInBlock(block.createdByLambda() ? false : block.returned(), block.breakedOrReturned(), !block.createdByLambda());
-		} else {
-			v.setReturnedInBlock(false, false, false);
-		}
-		return v;
-	}
+        RubyValue v = ((RubyProc)receiver).call(args);
+        block = ((RubyProc)receiver).getBlock();
+        if (null != block) {
+            v.setReturnedInBlock(block.createdByLambda() ? false : block.returned(), block.breakedOrReturned(), !block.createdByLambda());
+        } else {
+            v.setReturnedInBlock(false, false, false);
+        }
+        return v;
+    }
 }
 
 class Proc_arity extends RubyNoArgMethod {
-	protected RubyValue run(RubyValue receiver, RubyBlock block) {
-		RubyBlock b = ((RubyProc)receiver).getBlock();
-		return ObjectFactory.createFixnum(b.arity());
-	}
+    protected RubyValue run(RubyValue receiver, RubyBlock block) {
+        RubyBlock b = ((RubyProc)receiver).getBlock();
+        return ObjectFactory.createFixnum(b.arity());
+    }
 
 }
 
 class Proc_alloc extends RubyNoArgMethod {
-	protected RubyValue run(RubyValue receiver, RubyBlock block) {
-		return ObjectFactory.createProc(block);
-	}
+    protected RubyValue run(RubyValue receiver, RubyBlock block) {
+        return ObjectFactory.createProc(block);
+    }
+}
+
+class Proc_equal extends RubyOneArgMethod {
+    protected RubyValue run(RubyValue receiver, RubyValue arg, RubyBlock block) {
+        if (receiver.equals(arg)) {
+            return ObjectFactory.TRUE_VALUE;
+        } else {
+            return ObjectFactory.FALSE_VALUE;
+        }
+    }
 }
 
 public class ProcClassBuilder {
-	public static void initialize() {
-		RubyClass c = RubyRuntime.ProcClass;
-		RubyMethod call = new Proc_call();
-		c.defineMethod("call", call);
-		c.defineMethod("[]", call);
-		c.defineMethod("arity", new Proc_arity());
-		c.defineAllocMethod(new Proc_alloc());
-	}
+    public static void initialize() {
+        RubyClass c = RubyRuntime.ProcClass;
+        RubyMethod call = new Proc_call();
+        c.defineMethod("call", call);
+        c.defineMethod("[]", call);
+        c.defineMethod("arity", new Proc_arity());
+        c.defineMethod("==", new Proc_equal());
+        c.defineAllocMethod(new Proc_alloc());
+    }
 }
