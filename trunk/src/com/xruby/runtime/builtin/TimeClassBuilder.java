a268dc80d2e00d9fc2dc6346197ae554b647eb18
mechiland <mechiland@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-06-07 07:51:07 +0000

    the test_time passed

--- trunk/src/com/xruby/runtime/builtin/TimeClassBuilder.java
+++ trunk/src/com/xruby/runtime/builtin/TimeClassBuilder.java
@@ -5,11 +5,26 @@
 
 package com.xruby.runtime.builtin;
 
-import com.xruby.runtime.lang.*;
-import com.xruby.runtime.value.*;
-
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
+
+import com.xruby.runtime.lang.CommonRubyID;
+import com.xruby.runtime.lang.RubyBlock;
+import com.xruby.runtime.lang.RubyClass;
+import com.xruby.runtime.lang.RubyException;
+import com.xruby.runtime.lang.RubyMethod;
+import com.xruby.runtime.lang.RubyNoArgMethod;
+import com.xruby.runtime.lang.RubyOneArgMethod;
+import com.xruby.runtime.lang.RubyRuntime;
+import com.xruby.runtime.lang.RubyValue;
+import com.xruby.runtime.lang.RubyVarArgMethod;
+import com.xruby.runtime.value.ObjectFactory;
+import com.xruby.runtime.value.RubyArray;
+import com.xruby.runtime.value.RubyBignum;
+import com.xruby.runtime.value.RubyFixnum;
+import com.xruby.runtime.value.RubyFloat;
+import com.xruby.runtime.value.RubyTime;
 
 class Time_new extends RubyNoArgMethod {
     protected RubyValue run(RubyValue receiver, RubyBlock block) {
@@ -34,7 +49,7 @@ class Time_to_i extends RubyNoArgMethod {
 class Time_usec extends RubyNoArgMethod {
     protected RubyValue run(RubyValue receiver, RubyBlock block) {
         RubyTime t = (RubyTime) receiver;
-        return ObjectFactory.createFixnum((int) (t.getTime() * 1000));
+        return ObjectFactory.createFixnum(t.getUsec());
     }
 }
 
@@ -48,17 +63,17 @@ class Time_to_s extends RubyNoArgMethod {
 class Time_at extends RubyOneArgMethod {
     protected RubyValue run(RubyValue receiver, RubyValue arg, RubyBlock block) {
         Object value = arg;
-        long time = 0;
+        double time = 0;
         if (value instanceof RubyFixnum) {
             time = ((RubyFixnum) value).intValue();
         } else if (value instanceof RubyBignum) {
             time = ((RubyBignum) value).getInternal().longValue();
         } else if (value instanceof RubyFloat) {
-            time = (long) ((RubyFloat) value).doubleValue();
+            time = ((RubyFloat) value).doubleValue();
         } else {
             throw new RubyException(RubyRuntime.TypeErrorClass, "can't convert " + arg.getRubyClass().getName() + " into Time");
         }
-        return ObjectFactory.createTime(new Date(time * 1000));
+        return ObjectFactory.createTime(new Date((long) (time*1000)));
     }
 }
 
@@ -66,27 +81,44 @@ class Time_plus extends RubyOneArgMethod {
     protected RubyValue run(RubyValue receiver, RubyValue arg, RubyBlock block) {
         RubyTime time = (RubyTime) receiver;
         Object value = arg;
-        long timeAdd = 0;
+        double timeAdd = 0;
         if (value instanceof RubyFixnum) {
             timeAdd = ((RubyFixnum) value).intValue();
         } else if (value instanceof RubyBignum) {
             timeAdd = ((RubyBignum) value).getInternal().longValue();
         } else if (value instanceof RubyFloat) {
-            double add = (long) ((RubyFloat) value).doubleValue();
-            return ObjectFactory.createTime(new Date(time.getTime() + (long) (add * 1000)));
+            timeAdd = ((RubyFloat) value).doubleValue();
         } else {
             throw new RubyException(RubyRuntime.TypeErrorClass, "can't convert " + arg.getRubyClass().getName() + " into Float");
         }
-        return ObjectFactory.createTime(new Date(time.getTime() + timeAdd * 1000));
+        return ObjectFactory.createTime(new Date(time.getTime() + (long)(timeAdd * 1000)));
     }
 }
 
 class Time_minus extends RubyOneArgMethod {
     protected RubyValue run(RubyValue receiver, RubyValue arg, RubyBlock block) {
         RubyTime time1 = (RubyTime) receiver;
-        RubyTime time2 = RubyTypesUtil.convertToTime(arg);
-        long timeInteval = time1.getTime() - time2.getTime();
-        return ObjectFactory.createFloat((double) timeInteval / 1000);
+        if (arg instanceof RubyTime) {
+            RubyTime time2 = RubyTypesUtil.convertToTime(arg);
+            long timeInteval = time1.getTime() - time2.getTime();
+            if (timeInteval % 1000 == 0) {
+                return ObjectFactory.createFixnum((int) (timeInteval / 1000));
+            }
+            return ObjectFactory.createFloat((double) timeInteval / 1000);
+        }
+        
+        Object value = arg;
+        double time = 0;
+        if (value instanceof RubyFixnum) {
+            time = ((RubyFixnum) value).intValue();
+        } else if (value instanceof RubyBignum) {
+            time = ((RubyBignum) value).getInternal().longValue();
+        } else if (value instanceof RubyFloat) {
+            time = ((RubyFloat) value).doubleValue();
+        } else {
+            throw new RubyException(RubyRuntime.TypeErrorClass, "can't convert " + arg.getRubyClass().getName() + " into Numberic");
+        }
+        return ObjectFactory.createTime(new Date((time1.getTime() - (long)(time * 1000))));
     }
 }
 
@@ -116,9 +148,9 @@ class Time_utc extends RubyVarArgMethod {
         int hour = (args.size() <= i) ? 0 : ((RubyFixnum) args.get(i++)).intValue();
         int min = (args.size() <= i) ? 0 : ((RubyFixnum) args.get(i++)).intValue();
         int sec = (args.size() <= i) ? 0 : ((RubyFixnum) args.get(i++)).intValue();
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day, hour, min, sec);
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        calendar.clear();
+        calendar.set(year, month-1, day, hour, min, sec); 
         return ObjectFactory.createTime(calendar.getTime());
     }
 }
--- trunk/src/com/xruby/runtime/value/RubyTime.java
+++ trunk/src/com/xruby/runtime/value/RubyTime.java
@@ -10,7 +10,7 @@ import java.util.Date;
 import com.xruby.runtime.lang.*;
 
 public class RubyTime extends RubyBasic {
-	private Date date_;
+	private final Date date_;
 	
 	RubyTime(Date date){
 		super(RubyRuntime.TimeClass);
@@ -25,9 +25,22 @@ public class RubyTime extends RubyBasic {
 	public long getTime() {
 		return date_.getTime();
 	}
+    
+    public int getUsec() {
+        long t = getTime();
+        
+        if (t > 0 && t < 1000) {
+            return (int) (t * 1000);
+        }
+        
+        float t1 = ((float)t / 1000);
+        float t2 = (long)t1;
+        return (int)((t1 - t2) * 1000000);
+    }
 	
 	public String toString() {
 		SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss ZZZZZ yyyy");
 		return sdf.format(date_);
 	}
+   
 }
