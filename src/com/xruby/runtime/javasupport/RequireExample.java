6b5d4f7d1ae685883f299a99e276816a3838fad6
zhangyu8374 <zhangyu8374@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-06-20 08:31:46 +0000

    add some test examples

--- src/com/xruby/runtime/javasupport/RequireExample.java
+++ src/com/xruby/runtime/javasupport/RequireExample.java
@@ -19,15 +19,59 @@ public class RequireExample {
         String program_text = "" +
                 "import 'java.util.*'\n" +
                 "r = Random.new" + "\n" +
+                //call overloaded methods
                 "puts r.nextInt\n" +
                 "puts r.nextInt(3)\n" +
+                //call methods inherited superclass(for example:Object)
+                "puts r.toString\n" +
+                
+                
+                //call static methods and fields
+                "import 'java.lang.Math'\n"+
+                "puts JMath.PI\n"+
+                "puts JMath.cos(0)\n"+
+                
+                
+                //access Java constant variables
+                "import 'java.lang.System'\n"+
+                "out = System::out\n"+
+                "out.println('ok!')\n"+
+                
+                
+                //escape from name collision
+                "import 'java.lang.Object'\n"+
+                "o = JObject.new\n" +
+                "puts o.toString\n" +
+                
+                
+                //implement a Java interface
+                "require_java 'java.lang.Runnable'\n"+
+                "require_java 'java.lang.Thread'\n"+
+                "class MyRunnable < Runnable\n"+
+                "   def run\n"+
+                "       puts 'ok'\n"+
+                "   end\n"+
+                "end\n"+
+                "r = MyRunnable.new\n"+
+                "thread = JThread.new(r)\n"+
+                "thread.start()\n"+
+
                 "array = ArrayList.new\n" +
                 "array.add 1\n" +
                 "array.add 2\n" +
                 "array.add 3\n" +
                 "for i in 1 ... 3\n" +
                 "    puts array.get(i)\n" +
-                "end";
+                "end\n"+
+                
+                //JavaBean support
+                "import 'javax.swing.JFrame'\n"+
+                "f = JFrame.new('hello')\n"+
+                //calls setVisible
+                "f.visible= true\n"+
+                //calls getTitle
+                "puts f.title\n"+
+                "\n";
         
         RubyCompiler compiler = new RubyCompiler(null, false);
 		CompilationResults codes = compiler.compile(new StringReader(program_text));
