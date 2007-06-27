1da61c96fd1334c049b421affbca0c22bff96c8e
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-06-27 03:30:52 +0000

    version 0.2.1

--- trunk/src/com/xruby/compiler/RubyCompiler.java
+++ trunk/src/com/xruby/compiler/RubyCompiler.java
@@ -21,7 +21,7 @@ import java.io.Reader;
 
 public class RubyCompiler {
 
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.2.1";
 
     private RubyBinding binding_;
     private boolean strip_;
