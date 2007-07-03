16f82383356619dbedba219ede59692fc6269f68
femtowin <femtowin@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-07-03 07:01:45 +0000

    porting from jruby's parser implementation to our here, just expect things can't compile for a while in this branch.

---
+++ branches/yacc/src/com/xruby/compiler/parser/RubyJayTest.java
@@ -0,0 +1,16 @@
+package com.xruby.compiler.parser;
+
+import junit.framework.TestCase;
+
+/**
+ * Copyright 2005-2007 femto
+ * Distributed under the GNU General Public License 2.0
+ */
+public class RubyJayTest extends TestCase {
+    public void test_something() throws Exception {
+        new RubyJay();
+    }
+    public void test_lexer() throws Exception {
+        new RubyYaccLexer();
+    }
+}
