649278becfbed1e1d6c6c4647ad5830c495a4a60
femtowin <femtowin@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-07-03 06:36:28 +0000

    JayGenerator

---
+++ branches/yacc/src/com/xruby/compiler/parser/JayGenerator.java
@@ -0,0 +1,17 @@
+package com.xruby.compiler.parser;
+
+import java.io.PrintStream;
+
+/**
+ * Copyright 2005-2007 femto
+ * Distributed under the GNU General Public License 2.0
+ */
+public class JayGenerator {
+    public static void main(String[] args) throws Exception {
+        //System.out.println(System.getProperty("user.dir"));
+        //System.setProperty("user.dir", "C:\\toolset\\ruby\\xruby-trunk\\");
+        System.setOut(new PrintStream("src\\com\\xruby\\compiler\\parser\\RubyJay.java"));
+        args = new String[] {"-v", "-t", "-b", "output1", "src\\com\\xruby\\compiler\\parser\\jay.y"};
+        jay.Main.main(args);
+    }
+}
