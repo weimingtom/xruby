009847841140a842a3a89335eee50d1c33438ad9
yuesefa <yuesefa@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-07-09 15:08:48 +0000

    back up

---
+++ trunk/src/com/xruby/compiler/parser/Rubyv3Lexer.java
@@ -0,0 +1,22 @@
+package com.xruby.compiler.parser;
+
+import java.util.Stack;
+import java.io.Reader;
+
+import org.antlr.runtime.*;
+
+
+
+public class Rubyv3Lexer extends Rubyv3BaseLexer {
+	
+	public Rubyv3Lexer(ANTLRInputStream input){
+		super(input);
+	}
+	
+	public Rubyv3Lexer(ANTLRStringStream input){
+		super(input);
+	}
+	
+	
+	
+}
