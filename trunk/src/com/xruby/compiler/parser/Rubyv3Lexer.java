12e986f99ba0c025f0b2958f932f8005c9ab18b4
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-08-05 05:51:34 +0000

    add Rubyv3Parser

--- trunk/src/com/xruby/compiler/parser/Rubyv3Lexer.java
+++ trunk/src/com/xruby/compiler/parser/Rubyv3Lexer.java
@@ -1,8 +1,9 @@
 package com.xruby.compiler.parser;
 
+import java.util.*;
 import org.antlr.runtime.*;
 
-public class Rubyv3Lexer extends Rubyv3BaseLexer {
+public class Rubyv3Lexer extends Rubyv3BaseLexer implements TokenStream {
 
     public Rubyv3Lexer(CharStream input, SymbolTableManager stm, boolean strip){
         super(input);
@@ -59,6 +60,93 @@ public class Rubyv3Lexer extends Rubyv3BaseLexer {
         default:
             return in;
         }
+    }
+
+    private List<Token> tokens = new ArrayList<Token>();
+    private int p = -1; //actually, no need for p=-1 for juding preload
+    private int initializedIndex = -1; //how far do we go?
+
+    public Token LT(int k) {
+        if (p + k > initializedIndex) {//need to initialize on
+            while (initializedIndex < p + k) {
+                Token token = nextToken();
+                initializedIndex++;
+                token.setTokenIndex(initializedIndex);
+                tokens.add(token);
+            }
+        }
+        /*if (k < 0) {
+            return LB(-k);
+        }*/
+        //System.out.println("p+k" + (p + k));
+        if (p + k < 0) {
+            return null;
+        }
+
+        return (Token) tokens.get(p + k);  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Token get(int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public TokenSource getTokenSource() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String toString(int arg0, int arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String toString(Token arg0, Token arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public int LA(int i) {
+        return LT(i).getType();
+    }
+
+    public void consume() {
+        p++;
+    }
+
+    public int index() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public int mark() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public void release(int arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void rewind() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void rewind(int arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void seek(int arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
     }
 
 
---
+++ trunk/src/com/xruby/compiler/parser/Rubyv3Parser.java
@@ -0,0 +1,10 @@
+package com.xruby.compiler.parser;
+
+import org.antlr.runtime.*;
+
+public class Rubyv3Parser extends Rubyv3BaseParser {
+    public Rubyv3Parser(TokenStream input) {
+        super(input);
+    }
+
+}
