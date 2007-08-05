381fa5a33d765862fd1212662c1f3b170f2f0c48
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-08-05 05:06:34 +0000

    pass symbol table to lexer

--- trunk/src/com/xruby/compiler/parser/Rubyv3Lexer.java
+++ trunk/src/com/xruby/compiler/parser/Rubyv3Lexer.java
@@ -1,76 +1,71 @@
 package com.xruby.compiler.parser;
 
-import java.util.Stack;
-import java.io.Reader;
-
 import org.antlr.runtime.*;
 
-
-
 public class Rubyv3Lexer extends Rubyv3BaseLexer {
-	
-	public Rubyv3Lexer(ANTLRInputStream input){
-		super(input);
-	}
-	
-	public Rubyv3Lexer(ANTLRStringStream input){
-		super(input);
-	}
-	
-	protected boolean isAsciiValueTerminator(char value) {
-		switch (value) {
-		case '\t':
-		case ' ':
-		case '\f':
-		case '\13':
-		case '\r':
-		case '\n':
-		case (char)Rubyv3Lexer.EOF:
-		case ')': // while(ARGV[0] && ARGV[0][0] == ?-)
-		case ',': // [?d, ?s, :date]
-		case ']': // [?a]
-		case ';': // $state[n*2] = ?|;
-		case '.': // unless (?A..?Z) === klass[0]
-			return true;
-		default:
-			return false;
-		}
-	}
-	
-	protected int trackDelimiterCount(char next_char, char delimeter, int delimeter_count) {
-		if (delimeter == translateDelimiter(delimeter)) {
-			if (delimeter == next_char) {
-				--delimeter_count;
-				assert (delimeter_count >= 0);
-			}
-		} else {
-			if (delimeter == next_char) {
-				++delimeter_count;
-			} else if (next_char == translateDelimiter(delimeter)) {
-				--delimeter_count;
-				assert (delimeter_count >= 0);
-			}
-		}
 
-		return delimeter_count;
-	}
+    public Rubyv3Lexer(ANTLRInputStream input, SymbolTableManager stm, boolean strip){
+        super(input);
+    }
 
-	private char translateDelimiter(char in) {
-		switch (in) {
-		case '{':
-			return '}';
-		case '[':
-			return ']';
-		case '(':
-			return ')';
-		case '<':
-			return '>';
-		default:
-			return in;
-		}
-	}
-	
-	
-	
-	
+    public Rubyv3Lexer(ANTLRStringStream input, SymbolTableManager stm, boolean strip){
+        super(input);
+    }
+
+    protected boolean isAsciiValueTerminator(char value) {
+        switch (value) {
+        case '\t':
+        case ' ':
+        case '\f':
+        case '\13':
+        case '\r':
+        case '\n':
+        case (char)Rubyv3Lexer.EOF:
+        case ')': // while(ARGV[0] && ARGV[0][0] == ?-)
+        case ',': // [?d, ?s, :date]
+        case ']': // [?a]
+        case ';': // $state[n*2] = ?|;
+        case '.': // unless (?A..?Z) === klass[0]
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    protected int trackDelimiterCount(char next_char, char delimeter, int delimeter_count) {
+        if (delimeter == translateDelimiter(delimeter)) {
+            if (delimeter == next_char) {
+                --delimeter_count;
+                assert (delimeter_count >= 0);
+            }
+        } else {
+            if (delimeter == next_char) {
+                ++delimeter_count;
+            } else if (next_char == translateDelimiter(delimeter)) {
+                --delimeter_count;
+                assert (delimeter_count >= 0);
+            }
+        }
+
+        return delimeter_count;
+    }
+
+    private char translateDelimiter(char in) {
+        switch (in) {
+        case '{':
+            return '}';
+        case '[':
+            return ']';
+        case '(':
+            return ')';
+        case '<':
+            return '>';
+        default:
+            return in;
+        }
+    }
+
+
+
+
 }
