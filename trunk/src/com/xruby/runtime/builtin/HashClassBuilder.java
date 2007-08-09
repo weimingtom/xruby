8fa6cdc93652da6b102fa0e1f38d159a6d2a551d
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-08-09 04:51:36 +0000

    apply annotation to RubyHash

--- trunk/src/com/xruby/runtime/builtin/HashClassBuilder.java
+++ trunk/src/com/xruby/runtime/builtin/HashClassBuilder.java
@@ -10,26 +10,6 @@ import com.xruby.runtime.value.ObjectFactory;
 import com.xruby.runtime.value.RubyArray;
 import com.xruby.runtime.value.RubyHash;
 
-class Hash_hash_access extends RubyVarArgMethod {
-    protected RubyValue run(RubyValue receiver, RubyArray args, RubyBlock block) {
-        RubyHash value = (RubyHash) receiver;
-        if (1 == args.size()) {
-            return value.get(args.get(0));
-        }
-
-        //TODO
-        throw new RubyException("not implemented");
-    }
-}
-
-class Hash_hash_set extends RubyVarArgMethod {
-    protected RubyValue run(RubyValue receiver, RubyArray args, RubyBlock block) {
-        RubyHash value = (RubyHash) receiver;
-        value.add(args.get(0), args.get(1));
-        return args.get(1);
-    }
-}
-
 class Hash_new extends RubyNoArgMethod {
     protected RubyValue run(RubyValue receiver, RubyBlock block) {
         RubyHash h = ObjectFactory.createHash();
@@ -42,8 +22,6 @@ public class HashClassBuilder {
 
     public static void initialize() {
         RubyClass c = RubyRuntime.HashClass;
-        c.defineMethod("[]", new Hash_hash_access());
-        c.defineMethod("[]=", new Hash_hash_set());
         c.defineAllocMethod(new Hash_new());
     }
 }
