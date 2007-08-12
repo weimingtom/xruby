ab906ef00c925e124e1d76c2a79043b242993af7
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-08-12 05:20:19 +0000

    apply annotation to RubyString

--- trunk/src/com/xruby/runtime/builtin/StringClassBuilder.java
+++ trunk/src/com/xruby/runtime/builtin/StringClassBuilder.java
@@ -9,47 +9,7 @@ import com.xruby.runtime.lang.*;
 import com.xruby.runtime.value.*;
 
 
-class String_operator_star extends RubyOneArgMethod {
-    protected RubyValue run(RubyValue receiver, RubyValue arg, RubyBlock block) {
-        String string = ((RubyString) receiver).toString();
-        int count = arg.toInt();
-        if (count < 0) {
-            throw new RubyException(RubyRuntime.ArgumentErrorClass, "negative argument");
-        }
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < count; ++i) {
-            result.append(string);
-        }
-        return ObjectFactory.createString(result);
-    }
-}
 
-class String_each_byte extends RubyNoArgMethod {
-    protected RubyValue run(RubyValue receiver, RubyBlock block) {
-        String string = ((RubyString) receiver).toString();
-        for (int i = 0; i < string.length(); ++i) {
-            char c = string.charAt(i);
-            block.invoke(receiver, ObjectFactory.createFixnum((int) c));
-        }
-        return receiver;
-    }
-}
-
-class String_reverse_danger extends RubyNoArgMethod {
-    protected RubyValue run(RubyValue receiver, RubyBlock block) {
-        RubyString string = (RubyString) receiver;
-        string.reverse();
-        return string;
-    }
-}
-
-class String_reverse extends RubyNoArgMethod {
-    protected RubyValue run(RubyValue receiver, RubyBlock block) {
-        RubyString string = ObjectFactory.createString(((RubyString) receiver).toString());
-        string.reverse();
-        return string;
-    }
-}
 
 class String_chomp extends RubyVarArgMethod {
     protected RubyValue run(RubyValue receiver, RubyArray args, RubyBlock block) {
@@ -185,10 +145,6 @@ class String_count extends RubyVarArgMethod {
 public class StringClassBuilder {
     public static void initialize() {
         RubyClass c = RubyRuntime.StringClass;
-        c.defineMethod("*", new String_operator_star());
-        c.defineMethod("each_byte", new String_each_byte());
-        c.defineMethod("reverse!", new String_reverse_danger());
-        c.defineMethod("reverse", new String_reverse());
         c.defineMethod("chomp", new String_chomp());
         c.defineMethod("chomp!", new String_chomp_danger());
         c.defineMethod("scan", new String_scan());
