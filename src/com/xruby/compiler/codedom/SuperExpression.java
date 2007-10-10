0a8b71bc84d1abd9e0e8ced88496c90bb8b0710b
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-10-10 01:55:53 +0000

    refactoring (more changes coming for 'super')

--- src/com/xruby/compiler/codedom/SuperExpression.java
+++ src/com/xruby/compiler/codedom/SuperExpression.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * Copyright 2005-2007 Xue Yong Zhi
  * Distributed under the GNU General Public License 2.0
  */
@@ -7,39 +7,55 @@ package com.xruby.compiler.codedom;
 
 public class SuperExpression extends Expression {
 
-	private MethodCallArguments arguments_;
-	private Block block_;
+    private MethodCallArguments arguments_;
+    private Block block_;
 
-	public SuperExpression(MethodCallArguments arguments, Block block) {
-		arguments_ = arguments;
-		block_ = block;
-	}
+    public SuperExpression(MethodCallArguments arguments, Block block) {
+        arguments_ = arguments;
+        block_ = block;
+    }
 
-	public void accept(CodeVisitor visitor) {
-		visitor.visitSuperBegin();
+    public void accept(CodeVisitor visitor) {
+        if (null == arguments_) {
+            acceptSuperWithImplicitParameter(visitor);
+        } else {
+            acceptSuperWithExplicitParameter(visitor);
+        }
+    }
 
-		final boolean single_arg = (null != arguments_) && 
-									(arguments_.size() == 1) &&
-									(null == arguments_.getAsteriskArgument());
+    private void acceptSuperWithImplicitParameter(CodeVisitor visitor) {
+        visitor.visitSuperBegin();
+        visitor.visitNoParameterForSuper();
+        acceptBlock(visitor);
+        visitor.visitSuperEnd(true, false);
+    }
 
-		if (null == arguments_) {
-			visitor.visitNoParameterForSuper();
-		} else if (single_arg) {
-			arguments_.getFirstExpression().accept(visitor);
-		} else {
-			arguments_.accept(visitor);
-		}
+    private void acceptSuperWithExplicitParameter(CodeVisitor visitor) {
+        visitor.visitSuperBegin();
 
-		if (null != block_) {
-			block_.accept(visitor);
-		} else if (null != arguments_ && null != arguments_.getBlockArgument()) {
-			arguments_.getBlockArgument().accept(visitor);
-			visitor.visitBlockArgument();
-		} else {
-			visitor.visitNoBlock(true);
-		}
+        final boolean single_arg = (arguments_.size() == 1) &&
+                                    (null == arguments_.getAsteriskArgument());
 
-		visitor.visitSuperEnd(null == arguments_, single_arg);
-	}
+        if (single_arg) {
+            arguments_.getFirstExpression().accept(visitor);
+        } else {
+            arguments_.accept(visitor);
+        }
+
+        acceptBlock(visitor);
+
+        visitor.visitSuperEnd(null == arguments_, single_arg);
+    }
+
+    private void acceptBlock(CodeVisitor visitor) {
+        if (null != block_) {
+            block_.accept(visitor);
+        } else if (null != arguments_ && null != arguments_.getBlockArgument()) {
+            arguments_.getBlockArgument().accept(visitor);
+            visitor.visitBlockArgument();
+        } else {
+            visitor.visitNoBlock(true);
+        }
+    }
 
 }
