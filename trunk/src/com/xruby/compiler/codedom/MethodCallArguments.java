954d647984804e1763527b98c9d23f5be78b58bb
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-10-25 04:38:45 +0000

    fix a VerifyError

--- trunk/src/com/xruby/compiler/codedom/MethodCallArguments.java
+++ trunk/src/com/xruby/compiler/codedom/MethodCallArguments.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * Copyright 2005-2007 Xue Yong Zhi
  * Distributed under the GNU General Public License 2.0
  */
@@ -9,71 +9,77 @@ import java.util.ArrayList;
 
 public class MethodCallArguments implements Visitable {
 
-	protected final ArrayList<Expression> arguments_ = new ArrayList<Expression>();
-	protected Expression asterisk_argument_ = null;
-	protected Expression block_argument_ = null;
-	
-	public MethodCallArguments() {
-	}
-	
-	public MethodCallArguments(Expression argument) {
-		arguments_.add(argument);
-	}
+    protected final ArrayList<Expression> arguments_ = new ArrayList<Expression>();
+    protected Expression asterisk_argument_ = null;
+    protected Expression block_argument_ = null;
 
-	Expression getAsteriskArgument() {
-		return asterisk_argument_;
-	}
+    public MethodCallArguments() {
+    }
 
-	Expression getBlockArgument() {
-		return block_argument_;
-	}
-	
-	public int size() {
-		return arguments_.size();
-	}
+    public MethodCallArguments(Expression argument) {
+        arguments_.add(argument);
+    }
 
-	Expression getFirstExpression() {
-		return arguments_.get(0);
-	}
-	
-	Expression getSecondExpression() {
-		return arguments_.get(1);
-	}
+    Expression getAsteriskArgument() {
+        return asterisk_argument_;
+    }
 
-	public MethodCallArguments clone() {
-		MethodCallArguments r = new MethodCallArguments();
-		for (Expression exp : arguments_) {
-			r.addArgument(exp);
-		}
-		return r;
-	}
+    Expression getBlockArgument() {
+        return block_argument_;
+    }
 
-	public void setAsteriskArgument(Expression argument) {
-		assert(null == asterisk_argument_);
-		asterisk_argument_ = argument;
-	}
+    public int size() {
+        return arguments_.size();
+    }
 
-	public void setBlockArgument(Expression argument) {
-		assert(null == block_argument_);
-		block_argument_ = argument;
-	}
+    Expression getFirstExpression() {
+        return arguments_.get(0);
+    }
 
-	public void addArgument(Expression argument) {
-		arguments_.add(argument);
-	}
+    Expression getSecondExpression() {
+        return arguments_.get(1);
+    }
 
-	public void accept(CodeVisitor visitor) {
-		if (arguments_.isEmpty() && null == asterisk_argument_) {
-			visitor.visitNoParameter();
-		} else {
-			ArrayExpression to_a = new ArrayExpression(arguments_, asterisk_argument_);
-			to_a.accept(visitor);
-		}
-	}
+    public MethodCallArguments clone() {
+        MethodCallArguments r = new MethodCallArguments();
+        for (Expression exp : arguments_) {
+            r.addArgument(exp);
+        }
+        return r;
+    }
 
-	public void getFrequentlyUsedIntegers(ArrayList<Integer> result) {
-		for (Expression exp : arguments_) {
-			exp.getFrequentlyUsedIntegers(result);
-		}
-	}
+    public void setAsteriskArgument(Expression argument) {
+        assert(null == asterisk_argument_);
+        asterisk_argument_ = argument;
+    }
+
+    public void setBlockArgument(Expression argument) {
+        assert(null == block_argument_);
+        block_argument_ = argument;
+    }
+
+    public void addArgument(Expression argument) {
+        arguments_.add(argument);
+    }
+
+    public void accept(CodeVisitor visitor) {
+        if (arguments_.isEmpty() && null == asterisk_argument_) {
+            visitor.visitNoParameter();
+        } else {
+            ArrayExpression to_a = new ArrayExpression(arguments_, asterisk_argument_);
+            to_a.accept(visitor);
+        }
+    }
+
+    public void getFrequentlyUsedIntegers(ArrayList<Integer> result) {
+        for (Expression exp : arguments_) {
+            exp.getFrequentlyUsedIntegers(result);
+        }
+    }
+
+    void getNewlyAssignedVariables(ISymbolTable symboltable, ArrayList<String> result) {
+        for (Expression exp : arguments_) {
+            exp.getNewlyAssignedVariables(symboltable, result);
+        }
+    }
 }
