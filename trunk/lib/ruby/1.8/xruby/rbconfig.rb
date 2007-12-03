9c0790e4c0abc9776e3798f4acfb79ab5d3c22df
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2007-12-03 14:32:02 +0000

    extend rbconfig.rb

--- trunk/lib/ruby/1.8/xruby/rbconfig.rb
+++ trunk/lib/ruby/1.8/xruby/rbconfig.rb
@@ -2,12 +2,38 @@
 #TODO add configs for XRuby
 
 module Config
-  RUBY_VERSION == "1.8.5" or
-    raise "ruby lib version (1.8.5) doesn't match executable version (#{RUBY_VERSION})"
+  #RUBY_VERSION == "1.8.5" or
+  #  raise "ruby lib version (1.8.5) doesn't match executable version (#{RUBY_VERSION})"
 
   TOPDIR = File.dirname(__FILE__).chomp!("/lib/ruby/1.8")
   DESTDIR = TOPDIR && TOPDIR[/\A[a-z]:/i] || '' unless defined? DESTDIR
   CONFIG = {}
+  
+  #TODO add more as needed
+  CONFIG["sitedir"] = "$(prefix)/lib/ruby/site_ruby"
+  
+  MAKEFILE_CONFIG = {}
+  CONFIG.each{|k,v| MAKEFILE_CONFIG[k] = v.dup}
+  def Config::expand(val, config = CONFIG)
+    val.gsub!(/\$\$|\$\(([^()]+)\)|\$\{([^{}]+)\}/) do |var|
+      if !(v = $1 || $2)
+	'$'
+      elsif key = config[v = v[/\A[^:]+(?=(?::(.*?)=(.*))?\z)/]]
+	pat, sub = $1, $2
+	config[v] = false
+	Config::expand(key, config)
+	config[v] = key
+	key = key.gsub(/#{Regexp.quote(pat)}(?=\s|\z)/n) {sub} if pat
+	key
+      else
+	var
+      end
+    end
+    val
+  end
+  CONFIG.each_value do |val|
+    Config::expand(val)
+  end
 end
 RbConfig = Config # compatibility for ruby-1.9
 CROSS_COMPILING = nil unless defined? CROSS_COMPILING
