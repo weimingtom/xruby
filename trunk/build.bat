7188b7044cedd674af6eda252e6092c868291c54
xue.yong.zhi <xue.yong.zhi@9fc7faa1-f31c-0410-a540-e5ad5645f401>
2008-05-10 03:20:56 +0000

    ant 1.7.0

--- trunk/build.bat
+++ trunk/build.bat
@@ -1,2 +1,2 @@
 @echo off
-java -cp lib/ant/ant-1.6.5.jar;lib/ant/ant-launcher-1.6.5.jar;lib/ant/ant-antlr-1.6.5.jar;lib/ant/ant-junit-1.6.5.jar;lib/junit/junit-3.8.1.jar;"%JAVA_HOME%/lib/tools.jar" org.apache.tools.ant.Main %1
+java -cp lib/ant/ant-1.7.0.jar;lib/ant/ant-launcher-1.7.0.jar;lib/ant/ant-antlr-1.7.0.jar;lib/ant/ant-junit-1.7.0.jar;lib/junit/junit-3.8.1.jar;"%JAVA_HOME%/lib/tools.jar" org.apache.tools.ant.Main %1
